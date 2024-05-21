326ab1e4c3af23a9f81673fbdf62407bb205d8cf
gopalsinghh <149861736+gopalsinghh@users.noreply.github.com>
2024-05-21 13:37:45 +0530

    master country done

---
+++ backend/modules/master/controllers/CountryController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace backend\modules\master\controllers;
+
+use common\interfaces\StatusInterface;
+use common\models\master\country\form\MasterCountryForm;
+use common\models\master\country\MasterCountry;
+use common\models\master\country\MasterCountrySearch;
+
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+/**
+ * CountryController.
+ */
+class CountryController extends Controller
+{
+    /**
+     * Lists all MasterCenterSeatType models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new MasterCountrySearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+
+
+    /**
+     * Create SeatType.
+     * 
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new MasterCountryForm();
+        $model->status = StatusInterface::STATUS_ACTIVE;
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post())) {
+                if ($model->validate()) {
+                    $model->initializeForm();
+                    if ($model->country_model->save(false)) {
+                        \Yii::$app->session->setFlash('success', 'Data Submitted Successfully');
+                        return $this->redirect(['index']);
+                    }
+                }
+            }
+        } else {
+            $model->country_model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing MasterCenterSeatType model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $country_model = $this->findModel($id);
+        $model = new MasterCountryForm($country_model);
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post())) {
+                if ($model->validate()) {
+                    $model->initializeForm();
+                    if ($model->country_model->save()) {
+                        $model->country_model->save();
+                        \Yii::$app->session->setFlash('success', 'Data Updated Successfully');
+                        return $this->redirect(['index']);
+                    }
+                }
+            }
+        } else {
+            $model->country_model->loadDefaultValues();
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+
+
+    public function actionView($id)
+    {
+        $model = $this->findModel($id);
+
+
+        return $this->render('view', [
+            'model' => $model,
+        ]);
+    }
+
+
+    /**
+     * Deletes an existing MasterCenterSeatType model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $model = $this->findModel($id);
+        $model->country_name = $model->id . '_' . $model->country_name;
+        $model->status = StatusInterface::STATUS_DELETE;
+        $model->save();
+        \Yii::$app->session->setFlash('success', 'Data Updated Successfully');
+        return $this->redirect(\Yii::$app->request->referrer);
+    }
+
+    /**
+     * Finds the MasterCenterSeatType model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return MasterCenterSeatType the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = MasterCountry::findOne(['id' => $id, 'status' => [StatusInterface::STATUS_ACTIVE, StatusInterface::STATUS_SUSPEND]])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ common/models/master/country/form/MasterCountryForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace common\models\master\country\form;
+
+use Yii;
+use yii\base\Model;
+use common\models\GeneralModel;
+use common\models\master\country\MasterCountry;
+
+
+class MasterCountryForm extends model
+{
+    public $country_name;
+    public $status;
+    public $status_option = [];
+    public $country_model;
+
+
+    public function __construct(MasterCountry $country_model = null)
+    {
+
+        $this->country_model = Yii::createObject([
+            'class' => MasterCountry::className()
+        ]);
+
+        if ($country_model  != '') {
+            $this->country_model = $country_model;
+            $this->country_name = $this->country_model->country_name;
+            $this->status = $this->country_model->status;
+        }
+
+        $this->status_option = GeneralModel::statusoption();
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['country_name'], 'required'],
+            [['status'], 'integer'],
+            [['country_name'], 'string', 'max' => 255],
+            [['status'], 'default', 'value' => 1],
+
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'country_name' => 'Country',
+            'status' => 'Status',
+        ];
+    }
+    /**
+     * Initial Form Values
+     *
+     * @return void
+     */
+    public function initializeForm()
+    {
+        $this->country_model->country_name = $this->country_name;
+        $this->country_model->status = $this->status;
+    }
+}
