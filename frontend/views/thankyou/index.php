e412a8c387a431d9a896a529c84527405f90ecda
SMriti <smritipal2201@gmail.com>
2024-05-27 20:19:31 +0530

    changes done for thank you msg

--- frontend/views/thankyou/index.php
+++ frontend/views/thankyou/index.php
@@ -9,23 +9,23 @@ $this->params['title'] = $this->title;
 ?>
 
 <section class="thankyoupage ">
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-8">
-          <a href="/">
+      <div class="col-lg-8">
+        <a href="/">
           <div class="card_tankyou">
-                   <div class="tanksHeading">
-                    <h3 class="text-center pt-4">Thank you</h3>
-                   </div>
-                   <div class="content_wrp text-center">
-                    <p>Thank tou for Visiting <span>Walk into the Wild</span> you will <br> received an Email message Shortly!</p>
-                   </div>
-                   <div class="checkImgs text-center">
-                   <img src="<?= $this->params['baseurl'] ?>/img/check-mark.png" alt="" width="200">
-                   </div>
+            <div class="tanksHeading">
+              <h3 class="text-center pt-4">Thank you</h3>
             </div>
-          </a>
-        </div>
+            <div class="content_wrp text-center">
+              <p>Thank you for sharing your details with <span>Walk Into The Wild.</span> You will <br> receive a confirmation email shortly!</p>
+            </div>
+            <div class="checkImgs text-center">
+              <img src="<?= $this->params['baseurl'] ?>/img/check-mark.png" alt="" width="200">
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
-</div>
+  </div>
 </section>
