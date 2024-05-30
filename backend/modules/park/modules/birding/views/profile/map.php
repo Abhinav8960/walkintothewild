c35c2bed6967d7e2c42423e012e484dcc1c38e26
SMriti <smritipal2201@gmail.com>
2024-05-31 01:04:17 +0530

    changes done for park map

--- backend/modules/park/modules/birding/views/profile/map.php
+++ backend/modules/park/modules/birding/views/profile/map.php
@@ -7,6 +7,7 @@ $this->title = 'Birding Park Map';
 $this->params['breadcrumbs_home_url'] = '#';
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['title'] = $this->title;
+
 ?>
 <div class="panel panel-primary tabs-style-2">
     <?= $this->render('@backend/modules/park/modules/birding/views/profile/_profile_navbar', ['birding_park' => $model, 'map_active' => 'active']) ?>
@@ -14,7 +15,20 @@ $this->params['title'] = $this->title;
     <div class="panel-body tabs-menu-body main-content-body-right border">
         <div class="tab-content">
             <div class="tab-pane active">
+                <?php
 
+                $latitude = $model->latitude;
+                $longitude = $model->longitude;
+
+                $mapUrl = "https://www.google.com/maps?q={$latitude},{$longitude}&hl=es;z=14&output=embed";
+
+                if (!empty($latitude) && !empty($longitude)) {
+                ?>
+
+                    <iframe width="1500" height="450" frameborder="0" style="border:0" src="<?= $mapUrl ?>" allowfullscreen>
+                    </iframe>
+
+                <?php } ?>
             </div>
         </div>
     </div>
