09ec8855fb2af63b8211f23226b5da50a57bbd88
SMriti <smritipal2201@gmail.com>
2024-06-06 17:44:16 +0530

    changes done for rare and exotic animals

--- frontend/modules/park/views/default/rare_exotic.php
+++ frontend/modules/park/views/default/rare_exotic.php
@@ -1,5 +1,7 @@
 <?php
 
+use common\models\master\animal\MasterAnimal;
+use common\models\park\SafariParkAnimal;
 
 $webasset = $this->assetManager->getBundle('\frontend\assets\FrontAppAsset');
 $this->params['baseurl'] = $webasset->baseUrl;
@@ -15,30 +17,36 @@ if ($rare_exotics) {
             </div>
         </div>
     </div>
-    <?php foreach ($rare_exotics as $rare_exotic) { ?>
-        <div class="animal-safari mb-4">
-            <div class="inner_animals position-relative">
-                <img src="<?= $this->params['baseurl'] ?>/img/brownbearbg.jpg" alt="" class="position-relative w-100">
-                <div class="safariBox">
-                    <div class="container">
-                        <div class="row align-items-center ">
-                            <div class="col-lg-6 col-md-5 text-center position-relative">
-                                <img src="<?= $this->params['baseurl'] ?>/img/brownbear.png" alt="" class="imag_width position-relative">
-                            </div>
-                            <div class="col-lg-6 col-md-7">
-                                <div class="safari_content">
-                                    <h5>HIMALAYAN BROWN BEAR</h5>
-                                    <p>IUCN: CRITICALLY ENDANGERED</p>
-                                    <div class="knowmore">
-                                        <a href="" class="btn-knowmore">Know More</a>
+    <?php foreach ($rare_exotics as $rare_exotic) {
+        $safarianimal = SafariParkAnimal::find()->where(['safari_park_id' => $rare_exotic->id, 'animal_type_id' => 2, 'status' => 1])->limit(1)->orderBy(['id' => SORT_ASC])->one();
+        if ($safarianimal) {
+            $animal = MasterAnimal::find()->where(['status' => 1, 'id' => $safarianimal->master_animal_id])->limit(1)->one();
+    ?>
+            <div class="animal-safari mb-4">
+                <div class="inner_animals position-relative">
+                    <img src="<?= isset($animal->banner_image) ? $animal->bannerimagepath : $this->params['baseurl'] . '/img/brownbearbg.jpg' ?>" alt="" class="position-relative w-100">
+                    <div class="safariBox">
+                        <div class="container">
+                            <div class="row align-items-center ">
+                                <div class="col-lg-6 col-md-5 text-center position-relative">
+                                    <img src="<?= isset($animal->image) ? $animal->imagepath : $this->params['baseurl'] . '/img/brownbear.png' ?>" alt="" class="imag_width position-relative">
+                                </div>
+                                <div class="col-lg-6 col-md-7">
+                                    <div class="safari_content">
+                                        <h5><?= $animal->name ?></h5>
+                                        <p><?= $animal->short_description ?></p>
+                                        <div class="knowmore">
+                                            <a href="/park/<?= $rare_exotic->slug ?>" class="btn-knowmore">Know More</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
-    <?php } ?>
+    <?php }
+    }
+    ?>
 <?php } ?>
