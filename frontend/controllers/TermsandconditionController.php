542e3c70c96d79a53f07db14448e18b22094dd00
Gufran Ahmad <Gufran2307@gmail.com>
2024-06-14 10:57:05 +0530

    added termsandcondition page

---
+++ frontend/controllers/TermsandconditionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * DefaultController.
+ */
+class TermsandconditionController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render(
+            'index'
+        );
+    }
+}
