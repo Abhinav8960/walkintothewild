80b34f139b88a18b9c8e9be11c9ce814adf230a5
Gufran Ahmad <Gufran2307@gmail.com>
2024-06-25 17:41:57 +0530

    added 404page

--- frontend/views/site/_error_404.php
+++ frontend/views/site/_error_404.php
@@ -7,22 +7,15 @@
 /** @var Exception$exception */
 
 use yii\helpers\Html;
-
+$webasset = $this->assetManager->getBundle('\frontend\assets\FrontAppAsset');
+$this->params['baseurl'] = $webasset->baseUrl;
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="site-error errorPage" style="min-height: 700px;" >
+<div class="imgsbox pt-3">
+<a href="/"><img src="<?= $this->params['baseurl'] ?>/img/404 error with people holding the numbers-pana.svg" alt="" class="w-100"></a>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+</div>
+ <!-- <h1 class="title">404</h1>
+ <h3>We can't found the page you are looking for</h3> -->
 </div>
