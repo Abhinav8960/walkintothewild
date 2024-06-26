831d3183cfe61e7ca59815f49f0d066bc115599a
Akash Kumar <akashprajapatiak05@gmail.com>
2024-06-26 11:43:34 +0530

    highlight link

--- frontend/modules/article/views/default/_topic_search.php
+++ frontend/modules/article/views/default/_topic_search.php
@@ -10,9 +10,32 @@ $topics = MasterArticleTopic::find()->where(['status' => MasterArticleTopic::STA
         <?php if ($topics) {
             foreach ($topics as $topic) {
         ?>
-                <li><a href="<?= Url::toRoute(['/article/default/topic', 'slug' => $topic->slug]) ?>"><?= $topic->title ?> <i class="fa-solid fa-chevron-right"></i></a></li>
+                <li><a href="<?= Url::toRoute(['/article/default/topic', 'slug' => $topic->slug]) ?>" class="link"><?= $topic->title ?> <i class="fa-solid fa-chevron-right"></i></a></li>
 
         <?php }
         } ?>
     </ul>
 </div>
+
+
+
+<?php
+$script = <<<JS
+   var checkvalue= window.location.pathname;
+   $(".link").each(function(){
+    if($(this).attr('href')== checkvalue){ 
+    $(this).addClass("active");
+    }
+});
+JS;
+$this->registerJs($script);
+?>
+
+<style>
+    .link.active {
+        background-color: #09422D !important;
+        color: white !important;
+        border-radius: 5px !important;
+        margin: auto !important;
+    }
+</style>
