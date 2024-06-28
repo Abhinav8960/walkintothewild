b3f0a183265d889038e139544668d318bea52ff5
Aayush <aayushsaini999@gmail.com>
2024-06-28 15:05:58 +0530

    Safari : Operator Dashboard

--- backend/modules/operatordashboard/views/default/index.php
+++ backend/modules/operatordashboard/views/default/index.php
@@ -1,12 +1,59 @@
-<div class="operatordashboard-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php
+$this->title = 'Operator Dashboard';
+$this->params['breadcrumbs_home_url'] = '/operatordashboard';
+$this->params['breadcrumbs'][] =  ['label' => 'Operator', 'url' => '#'];
+$this->params['breadcrumbs'][] = $this->title;
+$this->params['title'] = $this->title;
+?>
+
+<div class="card">
+    <div class="card-body">
+        <div class="row row-sm">
+            <div class="col-lg-6 col-xl-3 col-md-6 col-12">
+                <a href="/operatordashboard/safari">
+                    <div class="card bg-primary-gradient text-white ">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mt-0 text-center">
+                                        <h2 class="text-white mb-0">Safari Tour</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6 col-xl-3 col-md-6 col-12">
+                <a href="/operatordashboard/birding">
+                    <div class="card bg-danger-gradient text-white">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mt-0 text-center">
+                                        <h2 class="text-white mb-0">Birding Tour</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6 col-xl-3 col-md-6 col-12">
+                <a href="#">
+                    <div class="card bg-warning-gradient text-white">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mt-0 text-center">
+                                        <h2 class="text-white mb-0">Resort/Londge/Home</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
---
+++ backend/modules/operatordashboard/views/safari/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$this->title = 'Safari Tour';
+$this->params['breadcrumbs_home_url'] = '/operatordashboard';
+$this->params['breadcrumbs'][] =  ['label' => 'Operator', 'url' => '#'];
+$this->params['breadcrumbs'][] = $this->title;
+$this->params['title'] = $this->title;
