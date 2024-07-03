07199ad7bc431320e0afcb27b499bce83104b66c
Gufran Ahmad <Gufran2307@gmail.com>
2024-07-03 10:29:39 +0530

    added css

--- frontend/modules/park/views/default/view.php
+++ frontend/modules/park/views/default/view.php
@@ -24,7 +24,7 @@ $banner = Banner::find()->where(['status' => 1, 'page_id' => $park_constant])->l
 <section class="banner_section-inner ee position-relative">
     <picture class="position-relative">
         <source srcset="<?= isset($banner->image) ? $banner->imagepath : $this->params['baseurl'] . '/img/NewBanner_big.png' ?>" media="(max-width:576px)" type="image/webp">
-        <img src="<?= isset($banner->image) ? $banner->imagepath : $this->params['baseurl'] . '/img/NewBanner_big.png' ?>" class="d-block w-100 banner_search" alt="banner">
+        <img src="<?= isset($banner->image) ? $banner->imagepath : $this->params['baseurl'] . '/img/NewBanner_big.png' ?>" class="d-block w-100 " alt="banner">
     </picture>
     <div class="banner_searchBox">
         <div class="container">
