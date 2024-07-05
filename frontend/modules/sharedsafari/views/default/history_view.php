63e942c6042be85f1533eea2f0ca582aa346dc75
Gufran Ahmad <Gufran2307@gmail.com>
2024-07-05 17:25:13 +0530

    changes tale

--- frontend/modules/sharedsafari/views/default/history_view.php
+++ frontend/modules/sharedsafari/views/default/history_view.php
@@ -8,12 +8,46 @@ $this->params['baseurl'] = $webasset->baseUrl;
 ?>
 <div class="row my-4">
     <div class="col-12">
-        <div class="wrapper-skybgsafri">
+        <div class="wrapper-skybgsafri p-2 pb-2">
             <?php
 
             if ($history_model) {
                 foreach ($history_model as $share_safari) { ?>
-                    <div class="row border_bottom2 pb-4">
+                <div class="table_design">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th class="th_history">Previous</th>
+                        <th class="th_history">New updated</th>
+                        </tr>
+                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="card card_history mb-1 h-100">
+                                     <h6><?= date('d M y', strtotime($share_safari->start_date)) ?> - <?= date('d M y', strtotime($share_safari->end_date)) ?></h6>
+                                     <h5 class="mb-0"><?= $share_safari->park->title ?></h5>
+                                     <p class="mb-0 pt-2">Organized by <strong><?= $share_safari->user->name ?> (Wildlife
+                                                Influencer)</strong></p>
+                                </div>
+                            </td>
+                            <td>
+                            <div class="card card_history mb-1 h-100">
+                                     <h6><?= date('d M y', strtotime($share_safari->start_date)) ?> - <?= date('d M y', strtotime($share_safari->end_date)) ?></h6>
+                                     <h5 class="mb-0"><?= $share_safari->park->title ?></h5>
+                                     <p class="mb-0 pt-2">Organized by <strong><?= $share_safari->user->name ?> (Wildlife
+                                                Influencer)</strong></p>
+                                </div>
+                            </td>
+                            
+                        </tr>
+                      
+                        
+                    </tbody>
+                </table>
+                </div>
+                    <!-- <div class="row border_bottom2 pb-4">
                         <div class="col-lg-7 border-right">
                             <div class="row">
                                 <div class="col-sm-2">
@@ -95,7 +129,7 @@ $this->params['baseurl'] = $webasset->baseUrl;
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
             <?php }
             } ?>
         </div>
