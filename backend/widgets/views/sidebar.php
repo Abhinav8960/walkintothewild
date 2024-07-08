ec2c0e3e4a5c218969e87bc7cbd03ebad419f15f
SMriti <smritipal2201@gmail.com>
2024-07-08 18:53:46 +0530

    changes done for side bar

--- backend/widgets/views/sidebar.php
+++ backend/widgets/views/sidebar.php
@@ -690,8 +690,7 @@ $active_url = "/" . Yii::$app->requestedRoute;
 					<li class="slide">
 						<a class="side-menu__item <?= in_array($active_url, array(
 														"/sharesafari/default/index",
-														"/sharesafari/default/index",
-														"/sharesafari/default/replies",
+														"/sharesafari/default/view",
 													)) ? "active" : "" ?>" href="/sharesafari/default/index"><img src="/img/carbon_workspace.png" alt="" width="25" height="25" class="navhover_icon"><span class="side-menu__label">Share Safari</span></a>
 					</li>
 
