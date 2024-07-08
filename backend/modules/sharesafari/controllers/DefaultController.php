b6de309edcb6909f329fe76af0abf3a086cfd065
SMriti <smritipal2201@gmail.com>
2024-07-08 18:51:41 +0530

    changes done for share safari

--- backend/modules/sharesafari/controllers/DefaultController.php
+++ backend/modules/sharesafari/controllers/DefaultController.php
@@ -3,7 +3,9 @@
 namespace backend\modules\sharesafari\controllers;
 
 use common\interfaces\StatusInterface;
+use common\models\sharesafari\ShareSafari;
 use common\models\sharesafari\ShareSafariComment;
+use common\models\sharesafari\ShareSafariCommentReport;
 use frontend\models\ShareSafariSearch;
 use Yii;
 use yii\web\Controller;
@@ -28,11 +30,46 @@ class DefaultController extends Controller
             'dataProvider' => $dataProvider,
         ]);
     }
+
     public function actionView($id)
     {
-        $comments = ShareSafariComment::find()->where(['share_safari_id' => $id, 'status' => StatusInterface::STATUS_ACTIVE])->all();
+        $share_safari = ShareSafari::find()
+            ->where([
+                'id' => $id,
+                'status' => StatusInterface::STATUS_ACTIVE,
+            ])->limit(1)->one();
         return $this->render('view', [
-            'comments' => $comments,
+            'share_safari' => $share_safari,
         ]);
+    }
+
+
+
+    public function actionFlag($slug, $park_id, $share_safari_comment_id)
+    {
+        $share_safari = ShareSafari::find()->where(['slug' => $slug])->one();
+        if (!$share_safari) {
+            return $this->redirect(['/sharesafari/default/index']);
+        }
+
+
+
+        $query = ShareSafariCommentReport::find()
+            ->where([
+                'share_safari_id' => $share_safari->id,
+                'share_safari_comment_id' => $share_safari_comment_id,
+                'park_id' => $park_id,
+            ]);
+        $dataProvider = new \yii\data\ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 100,
+            ],
+        ]);
+        if (Yii::$app->request->isAjax) {
+            return $this->renderAjax('_flag_list', [
+                'dataProvider' => $dataProvider,
+            ]);
+        }
     }
 }
