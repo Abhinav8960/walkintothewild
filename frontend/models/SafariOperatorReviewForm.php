39391849bede3d8bb5bb7f822da22c8f428a8c2d
Akash Kumar <akashprajapatiak05@gmail.com>
2024-07-08 14:09:18 +0530

    update of review

--- frontend/models/SafariOperatorReviewForm.php
+++ frontend/models/SafariOperatorReviewForm.php
@@ -2,10 +2,11 @@
 
 namespace frontend\models;
 
+use common\models\operator\SafariOperatorPark;
 use common\models\operator\SafariOperatorRating;
 use common\models\park\SafariPark;
 use Yii;
-
+use yii\helpers\ArrayHelper;
 
 class SafariOperatorReviewForm extends \yii\base\Model
 {
@@ -23,6 +24,14 @@ class SafariOperatorReviewForm extends \yii\base\Model
         $this->rating_model = Yii::createObject([
             'class' => SafariOperatorRating::className()
         ]);
+
+        if ($rating_model != null) {
+            $this->rating_model = $rating_model;
+            $this->safari_operator_id = $this->rating_model->safari_operator_id;
+            $this->park_id = $this->rating_model->park_id;
+            $this->rating = $this->rating_model->rating;
+            $this->review = $this->rating_model->review;
+        }
     }
 
     public function rules()
@@ -87,5 +96,24 @@ class SafariOperatorReviewForm extends \yii\base\Model
         $operator->google_rating = $avg;
         $operator->google_review_count = $count;
         $operator->save(false);
+    }
+
+    public function getParklist()
+    {
+        if ($this->rating_model->id) {
+            $user_park_id = SafariOperatorRating::find()->select('park_id')->where(['safari_operator_id' => $this->safari_operator_id, 'status' => 1])->andWhere("id <> " . $this->rating_model->id)->column();
+        } else {
+            $user_park_id = SafariOperatorRating::find()->select('park_id')->where(['safari_operator_id' => $this->safari_operator_id, 'status' => 1])->column();
+        }
+        $operator_safari_park = [];
+        $operatorsafariparkData = SafariOperatorPark::find()->where(['safari_operator_id' =>  $this->safari_operator_id, 'status' => 1])->andWhere(['not in', 'park_id', $user_park_id])->all();
+        if (count($operatorsafariparkData) >= 1) {
+
+            foreach ($operatorsafariparkData as $operatorsafaripark) {
+                $operator_safari_park[] = $operatorsafaripark->park_id;
+            }
+        }
+        $safariparkList =  SafariPark::find()->where(['in', 'id', $operator_safari_park]);
+        return ArrayHelper::map($safariparkList->orderBy(['title' => SORT_ASC])->all(), 'id', 'title');
     }
 }
