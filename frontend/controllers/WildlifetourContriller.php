a5cc9bdf49b9c0faf7254c02e883444808908ec0
Gufran Ahmad <Gufran2307@gmail.com>
2024-07-09 11:01:19 +0530

    chnages whildlife tour page

---
+++ frontend/controllers/WildlifetourContriller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace frontend\controllers;
+
+
+/**
+ * DefaultController.
+ */
+class WildlifetourController extends FrontendBaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render(
+            'index'
+        );
+    }
+}
