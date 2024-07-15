d8b94d897a82295cb19129c025c1f80cf1a410b9
Akash Kumar <akashprajapatiak05@gmail.com>
2024-07-15 16:04:36 +0530

    profile tab

--- frontend/modules/profile/views/default/index.php
+++ frontend/modules/profile/views/default/index.php
@@ -86,7 +86,15 @@ $this->params['title'] = $this->title;
         <div class="tab-pane fade show active" id="pills-followers" role="tabpanel" aria-labelledby="pills-followers-tab" tabindex="0">
             <div class="card">
                 <div class="card-body">
-                    Tab Content 1
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Perspiciatis maxime sunt mollitia modi voluptatum laboriosam aut quas beatae ratione illum repellendus,
+                    dolores voluptas labore doloribus obcaecati temporibus?
+                    Aspernatur excepturi tenetur quidem id.
+                    Harum maiores cupiditate exercitationem non laudantium soluta, animi veniam officia!
+                    Necessitatibus iure doloribus sed quam, quae mollitia possimus id aspernatur repudiandae veritatis deleniti eum eius laboriosam ad illo cumque, unde reiciendis veniam accusamus tenetur facere hic.
+                    Quasi deleniti quibusdam iusto aspernatur harum odio unde repudiandae cupiditate, accusantium porro soluta temporibus dolorum eos cum itaque expedita voluptates dolore maxime deserunt rerum.
+                    Culpa odit et quam natus qui cumque dolor laboriosam atque provident, laudantium tenetur dignissimos, consequatur minima tempore dolores.
+                    Officiis quia quibusdam enim autem, corporis accusamus tempore sed temporibus optio delectus cupiditate vel voluptas!
                 </div>
             </div>
         </div>
