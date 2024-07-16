a72f3150ba321ca346c681e23e9bfe035ef9abd6
SMriti <smritipal2201@gmail.com>
2024-07-16 19:43:42 +0530

    changes done

--- frontend/controllers/FrontendBaseController.php
+++ frontend/controllers/FrontendBaseController.php
@@ -20,14 +20,19 @@ class FrontendBaseController extends Controller
     /**
      * {@inheritdoc}
      */
-    public function afterAction($action, $result)
-    {
-        if (in_array($action->id, $this->action_ids)) {
-            $this->savePageViews();
-        }
+    // public function afterAction($action, $result)
+    // {
+    //     if (in_array($action->id, $this->action_ids)) {
+    //         $this->savePageViews();
+    //     }
 
-        return parent::afterAction($action, $result);
-    }
+    //     parent::afterAction($action, $result);
+
+    //     $event = new \yii\base\ActionEvent($action);
+    //     $event->result = $result;
+    //     $this->trigger(self::EVENT_AFTER_ACTION, $event);
+    //     return $event->result;
+    // }
 
     /**
      * Save Page Views
