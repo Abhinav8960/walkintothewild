90e404cd341216717cad33310e4b3bd597cb17f2
SMriti <smritipal2201@gmail.com>
2024-07-22 18:33:12 +0530

    changes done for safari

--- frontend/modules/park/views/default/_park_review.php
+++ frontend/modules/park/views/default/_park_review.php
@@ -87,11 +87,14 @@ $reviews = SafariParkRating::find()->where(['safari_park_id' => $safari_model->i
             } ?>
         </div>
     </div>
-    <div class="col-12">
-        <div class="safari text-center">
-            <div class="joinsafari">
-                <a href="/park/reviewlist/<?= $safari_model->slug ?>">View All</a>
+    <?php if (count($reviews) >= 5) { ?>
+
+        <div class="col-12">
+            <div class="safari text-center">
+                <div class="joinsafari">
+                    <a href="/park/reviewlist/<?= $safari_model->slug ?>">View All</a>
+                </div>
             </div>
         </div>
-    </div>
+    <?php } ?>
 </div>
