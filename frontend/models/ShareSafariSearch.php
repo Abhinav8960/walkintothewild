89dcd9d619f41937000631092ddce886cf090378
Akash Kumar <akashprajapatiak05@gmail.com>
2024-07-22 16:44:15 +0530

    approved

--- frontend/models/ShareSafariSearch.php
+++ frontend/models/ShareSafariSearch.php
@@ -50,7 +50,7 @@ class ShareSafariSearch extends ShareSafari
      */
     public function search($params, $pagination = true)
     {
-        $query = ShareSafari::find()->where(['share_safari.status' => [ShareSafari::STATUS_APPROVED, ShareSafari::STATUS_COMPLETED]])->andWhere(['>=', 'start_date', date("Y-m-d")]);
+        $query = ShareSafari::find()->where(['share_safari.status' => [ShareSafari::STATUS_APPROVED]])->andWhere(['>=', 'start_date', date("Y-m-d")]);
 
         // add conditions that should always apply here
 
