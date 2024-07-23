6f10832efd3560dbf0c259ed2209540152ad7282
Aayush <aayushsaini999@gmail.com>
2024-07-23 16:33:25 +0530

    Operator Packages

--- frontend/modules/manage/views/package/index.php
+++ frontend/modules/manage/views/package/index.php
@@ -1,12 +1,18 @@
 <?php
+
+use yii\helpers\Url;
+
 $this->title = $safari_operator->business_name . ' | Manage Operator Business';
 
 ?>
 
 <div class="container mt-5 mb-5">
     <div class="row mb-5">
-        <div class="col-md-12">
+        <div class="col-md-12 d-flex justify-content-between">
             <h5><?= $this->title ?></h5>
+            <div class="right_button float-md-end">
+                <a href="#<?= Url::toRoute(['/manage/package/create']) ?>" class="btn_newsafari packageBtn"><i class="fa fa-plus"></i> Create New Package</a>
+            </div>
         </div>
         <div class="col-md-3">
             <?= $this->render('@frontend/modules/manage/views/default/_sidebar', ['active' => 'package']); ?>
