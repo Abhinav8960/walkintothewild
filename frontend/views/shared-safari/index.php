f4e1ca7589104cf0440311aa49e7332761a17eb3
Akash Kumar <akashprajapatiak05@gmail.com>
2024-07-24 16:03:32 +0530

    profile url

--- frontend/views/shared-safari/index.php
+++ frontend/views/shared-safari/index.php
@@ -117,7 +117,7 @@ $banner = Banner::find()->where(['status' => 1, 'page_id' => $park_constant])->l
                                                 <div class="titleDate">
                                                     <h6><a href="<?= Url::toRoute(['/sharedsafari/default/view', 'slug' => $share_safari->slug]) ?>"><?= $share_safari->park->title ?></a></h6>
                                                     <div class="orgnizer">
-                                                        <p>Organized by: <strong><?= $share_safari->user->name ?></strong></p>
+                                                        <p>Organized by: <strong><?= $share_safari->organizedbyname ?></strong></p>
                                                     </div>
                                                 </div>
                                                 <div class="footer_card row pb-2 px-2 align-items-center">
