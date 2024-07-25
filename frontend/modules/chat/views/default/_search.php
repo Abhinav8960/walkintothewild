a0ef6e20fa2b18ef09bcae62d7e7614abfee0bbf
Akash Kumar <akashprajapatiak05@gmail.com>
2024-07-25 18:05:17 +0530

    search of user

---
+++ frontend/modules/chat/views/default/_search.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin([
+    'options' => [
+        'data-pjax' => true,
+        'id' => 'Searchform'
+    ],
+    'method' => 'get',
+    'action' => '/chat/default/index',
+]); ?>
+
+<?= $form->field($searchModel, 'name')->textInput(['class' => 'form-control', 'placeholder' => 'Search', 'autocomplete' => 'off'])->label(false) ?>
+<?php ActiveForm::end(); ?>
+
+<?php
+$js = <<<JS
+  $('form').on('change', function(){
+        $("#Searchform").attr("data-pjax", "true");    
+        $(this).closest('form').submit();
+    }); 
+    $('form input[type=text]').on('keyup', function(){
+        setTimeout(() => {
+            $("#Searchform").attr("data-pjax", "true");    
+            $(this).closest('form').submit();
+        }, 100);
+       
+    }); 
+
+JS;
+$this->registerJs($js);
+
+?>
