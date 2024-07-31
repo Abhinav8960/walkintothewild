6aa5227308b3a313351d8539a505ee56defa52a2
shokeenn <shokeen.triline@gmail.com>
2024-07-31 17:08:49 +0530

    add refer functionality

--- console/controllers/GenerateSiteXmlController.php
+++ console/controllers/GenerateSiteXmlController.php
@@ -64,7 +64,7 @@ class GenerateSiteXmlController extends Controller
 
         //get article site pages
         $additional_sitemap = [];
-        /*$additional_sitemap[] = $this->static_pages($backend_actual_url);
+        $additional_sitemap[] = $this->static_pages($backend_actual_url);
         $additional_sitemap[] = $this->get_park_site_pages($backend_actual_url);
         $additional_sitemap[] = $this->get_article_site_pages($backend_actual_url);
         $additional_sitemap[] = $this->get_article_category_site_pages($backend_actual_url);
@@ -78,8 +78,8 @@ class GenerateSiteXmlController extends Controller
             foreach ($all_url as $temp_url) {
                 $additional_sitemap[] = $temp_url;
             }
-        }*/
-        //$additional_sitemap[] = $this->get_monthly_package_site_pages($backend_actual_url);
+        }
+        $additional_sitemap[] = $this->get_monthly_package_site_pages($backend_actual_url);
         $additional_sitemap[] = $this->get_monthly_shared_safari_site_pages($backend_actual_url);
 
         //create site_index file
