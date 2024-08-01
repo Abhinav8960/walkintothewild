dc94c653125d299eaa58da13ad8eff88fc75c73c
Aayush <aayushsaini999@gmail.com>
2024-08-02 00:11:50 +0530

    Profile : Privacy page init

--- frontend/modules/account/views/privacy/index.php
+++ frontend/modules/account/views/privacy/index.php
@@ -1,11 +1,12 @@
 <?php
 $this->title = 'Account Settings';
+
 use yii\helpers\Url;
 ?>
 
 <div class="container mt-5 ">
     <div class="row margin_bottomfooter">
-    <div class="col-12 d-flex align-items-center justify-content-between mb-4">
+        <div class="col-12 d-flex align-items-center justify-content-between mb-4">
             <h6 class="fs-3 fw-bold ">Account Settings</h6>
             <a class="btn btn-info bg-blues py-2 rounded-5" href="<?= Url::toRoute(['/profile/default/index', 'user_handle' => Yii::$app->user->identity->user_handle]) ?>">View Profile</a>
         </div>
@@ -13,13 +14,30 @@ use yii\helpers\Url;
             <?= $this->render('@frontend/modules/account/views/default/_sidebar', ['active' => 'privacy']); ?>
         </div>
         <div class="col-md-9">
-        <div class="card account-settingside" style="min-height:500px">
+            <div class="card account-settingside" style="min-height:500px">
                 <div class="card-body p-4">
-                <h6 class="fs-5 fw-bold mb-3">  Privacy</h6>
+                    <h6 class="fs-5 fw-bold mb-3"> Select who may see your profile details</h6>
+
+
+                    <div class="row">
+                        <div class="col-md-12 mt-3">
+                            Gender
+                        </div>
+
+                        <div class="col-md-12 mt-5">
+                            Email
+                        </div>
+
+                        <div class="col-md-12 mt-5">
+                            Photo
+                        </div>
+
+                        <div class="col-md-12 mt-5">
+                            Contribution
+                        </div>
+                    </div>
                 </div>
-   
             </div>
         </div>
-       
     </div>
 </div>
