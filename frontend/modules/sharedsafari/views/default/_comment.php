4188c261126171a13b2e7afc200e16bf67e29403
Akash Kumar <akashprajapatiak05@gmail.com>
2024-08-03 17:52:14 +0530

    comment of fixed departure

--- frontend/modules/sharedsafari/views/default/_comment.php
+++ frontend/modules/sharedsafari/views/default/_comment.php
@@ -119,7 +119,7 @@ use yii\helpers\Url;
             <?php } else {
             if (Yii::$app->user->id) {
                 $share_safari_intrested = ShareSafariIntrested::find()->where(['user_id' => Yii::$app->user->identity->id, 'share_safari_id' => $share_safari->id, 'status' => 1])->limit(1)->one();
-                if ($share_safari_intrested || Yii::$app->user->id == $share_safari->host_user_id) { ?>
+                if ($share_safari_intrested || Yii::$app->user->id == $share_safari->host_user_id || $login_safarioperator && $share_safari->host_user_id == $login_safarioperator->id) { ?>
                     <?= $this->render('_comment_form', ['model' => $model]) ?>
         <?php } else {
                     echo '<p class="px-3 pt-2">Please Join in for start Comment</p>';
