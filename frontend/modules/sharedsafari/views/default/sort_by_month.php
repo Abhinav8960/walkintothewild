68834ba66d724360706a7e9d01ef12c1000b800c
Akash Kumar <akashprajapatiak05@gmail.com>
2024-08-06 17:06:43 +0530

    search of shared safari

--- frontend/modules/sharedsafari/views/default/sort_by_month.php
+++ frontend/modules/sharedsafari/views/default/sort_by_month.php
@@ -4,21 +4,7 @@ use common\models\GeneralModel;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/** @var yii\web\View $this */
-/** @var common\models\sharesafari\ShareSafari $model */
-/** @var yii\widgets\ActiveForm $form */
 ?>
-<?php $form = ActiveForm::begin([
-    'options' => [
-        'data-pjax' => true,
-        'id' => 'custom_sort_by_form'
-    ],
-    'action' => ['index'],
-    'method' => 'get',
-    'fieldConfig' => [
-        'template' => '{input}{error}',
-    ],
-]); ?>
 
 <div class="right-select">
     <div class="input_check pb-0 mb-3">
@@ -30,21 +16,8 @@ use yii\widgets\ActiveForm;
                 '4' => 'Cheapest',
 
 
-            ],['prompt'=>'Sort By : Created Recently']
+            ],
+            ['prompt' => 'Sort By : Created Recently']
         )->label(false); ?>
     </div>
 </div>
-<?php ActiveForm::end(); ?>
-
-<?php
-
-// $script = <<< JS
-
-//     $('form').on('change', function(){
-//         $("#side-search-form").attr("data-pjax", "true");    
-//         $(this).closest('form').submit();
-
-//     }); 
-// JS;
-// $this->registerJs($script);
-?>
