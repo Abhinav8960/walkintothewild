80feedd9dc86f12c7159895dd7128eb43b16b9f7
Aayush <aayushsaini999@gmail.com>
2024-08-14 15:09:49 +0530

    Profile Shared Safari

--- frontend/modules/profile/controllers/ShareSafariController.php
+++ frontend/modules/profile/controllers/ShareSafariController.php
@@ -2,10 +2,11 @@
 
 namespace frontend\modules\profile\controllers;
 
-use common\models\sharesafari\ShareSafari;
-use common\models\sharesafari\ShareSafariIntrested;
+use Yii;
 use common\models\User;
+use common\models\sharesafari\ShareSafari;
 use frontend\controllers\FrontendBaseController;
+use common\models\sharesafari\ShareSafariIntrested;
 
 
 /**
@@ -22,6 +23,17 @@ class ShareSafariController extends FrontendBaseController
         $user = $this->findUserbyHandle($user_handle);
         $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id]);
         $joined_by = ShareSafariIntrested::find()->where(['user_id' => $user->id]);
+
+        if (Yii::$app->user->identity) {
+            if ($user->id == Yii::$app->user->identity->id) {
+                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id]);
+                $joined_by = ShareSafariIntrested::find()->where(['user_id' => $user->id]);
+            } else {
+                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id])->andWhere(['>=', 'start_date', date("Y-m-d")]);
+                $joined_by = ShareSafariIntrested::find()->where(['user_id' => $user->id])->joinwith(['sharesafari'])->andWhere(['>=', 'start_date', date("Y-m-d")]);
+            }
+        }
+
         $dataProvider = new \yii\data\ActiveDataProvider([
             'query' => $organized_by,
             'pagination' => [
