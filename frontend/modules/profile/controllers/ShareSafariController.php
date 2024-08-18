94c69ae14516e0d6ff923abdc6ff2a1086b91f67
Vikas Lat-3500 <vikas.k.c@gmail.com>
2024-08-18 14:39:21 +0530

    profile share safari condition

--- frontend/modules/profile/controllers/ShareSafariController.php
+++ frontend/modules/profile/controllers/ShareSafariController.php
@@ -26,10 +26,10 @@ class ShareSafariController extends FrontendBaseController
 
         if (Yii::$app->user->identity) {
             if ($user->id == Yii::$app->user->identity->id) {
-                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id, 'type' => ShareSafari::TYPE_SAFARI]);
+                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id, 'type' => ShareSafari::TYPE_SAFARI,'status' => ShareSafari::STATUS_ACTIVE]);
                 $joined_by = ShareSafariIntrested::find()->where(['user_id' => $user->id]);
             } else {
-                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id, 'type' => ShareSafari::TYPE_SAFARI])->andWhere(['>=', 'start_date', date("Y-m-d")]);
+                $organized_by = ShareSafari::find()->where(['host_user_id' => $user->id, 'type' => ShareSafari::TYPE_SAFARI,'status' => ShareSafari::STATUS_ACTIVE])->andWhere(['>=', 'start_date', date("Y-m-d")]);
                 $joined_by = ShareSafariIntrested::find()->where(['user_id' => $user->id])->joinwith(['sharesafari'])->andWhere(['>=', 'start_date', date("Y-m-d")]);
             }
         }
