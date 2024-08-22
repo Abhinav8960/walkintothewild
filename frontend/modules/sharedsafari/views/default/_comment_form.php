6c293f336a41473aa657b1c212421a99ee4b1025
Gufran <gufran2307@gmail.com>
2024-08-22 14:30:58 +0530

    canges accordion design

--- frontend/modules/sharedsafari/views/default/_comment_form.php
+++ frontend/modules/sharedsafari/views/default/_comment_form.php
@@ -19,19 +19,19 @@ use yii\helpers\Url;
     </div>
 </div>
 <div class="comments-persons px-4 pt-2">
-<div class="row justify-content-center">
-    <div class="col-lg-12 col-xl-8">
-        <div class="post_text padding_ad">
-            <p>Commenting on this thread will notify all event attendees and will also be visible to
-                everyone viewing the event.</p>
+    <div class="row justify-content-center">
+        <div class="col-lg-12 col-xl-8">
+            <div class="post_text padding_ad">
+                <p>Commenting on this thread will notify all event attendees and will also be visible to
+                    everyone viewing the event.</p>
+            </div>
+        </div>
+        <div class="col-lg-12 col-xl-4 ">
+            <div class="comment_button float-end mb-lg-0 mb-3">
+                <?= Html::submitButton('Post Comment', ['class' => 'post-comment ']) ?>
+            </div>
         </div>
     </div>
-    <div class="col-lg-12 col-xl-4 ">
-        <div class="comment_button float-end mb-lg-0 mb-3">
-            <?= Html::submitButton('Post Comment', ['class' => 'post-comment ']) ?>
-        </div>
-    </div>
-</div>
 </div>
 
 <?php ActiveForm::end(); ?>
