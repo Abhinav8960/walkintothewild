d6437df35ae741bff4aff182aaca8e7596ccd246
Gufran <gufran2307@gmail.com>
2024-08-22 21:13:59 +0530

    safari view page

--- frontend/modules/sharedsafari/views/default/view.php
+++ frontend/modules/sharedsafari/views/default/view.php
@@ -79,7 +79,7 @@ $this->params['title'] = $this->title; ?>
                                         <div class="date_bx">
                                             <h6><?= date('d M y', strtotime($share_safari->start_date)) ?> - <?= date('d M y', strtotime($share_safari->end_date)) ?></h6>
                                         </div>
-                                        <h6 class="titler_safari"><a href="<?= Url::toRoute(['/park/default/view', 'slug' => $share_safari->park->slug]) ?>" data-pjax="0"><?= $share_safari->park->title ?></a></h6>
+                                        <h6 class="titler_safari"><a href="<?= Url::toRoute(['/park/default/view', 'slug' => $share_safari->park->slug]) ?>" data-pjax="0"><i class="fa-solid fa-location-dot me-1"></i> <?= $share_safari->park->title ?></a></h6>
                                         <?php
                                         if (Yii::$app->user->identity) { ?>
                                             <p class="mb-0 pt-1">Organized by <a href="<?= $share_safari->organizedbyprofileurl <> '' ? $share_safari->organizedbyprofileurl : '#' ?>" data-pjax="0"><strong><?= $share_safari->organizedbyname ?></strong></a></p><?php
