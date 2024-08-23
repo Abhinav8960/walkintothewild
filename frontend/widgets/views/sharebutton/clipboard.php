0a1001dc4d1d4e1caebd4e76a4f70fef284b3d93
Aayush <aayushsaini999@gmail.com>
2024-08-23 16:57:07 +0530

    Clipboard fixed in modal

--- frontend/widgets/views/sharebutton/clipboard.php
+++ frontend/widgets/views/sharebutton/clipboard.php
@@ -1,14 +1,14 @@
-<li class="copytoclipboard"><span class="iconSize copytoclipboardSpan" data-href="<?php echo preg_replace("/^http:/i", "https:", Yii::$app->request->absoluteUrl); ?>"><i class="fa-solid fa-link"></i></span></li>
-<li class="copytoclipboardModal"><span class="iconSize copytoclipboardModalSpan" data-href="<?php echo preg_replace("/^http:/i", "https:", Yii::$app->request->absoluteUrl); ?>"><i class="fa-solid fa-link"></i></span></li>
+<li><span class="iconSize copytoclipboard" data-href="<?php echo preg_replace("/^http:/i", "https:", Yii::$app->request->absoluteUrl); ?>"><i class="fa-solid fa-link"></i></span></li>
 
 <?php
 $script = <<< JS
 $(document).ready(function() {
     function copyToClipboard() {
-        $('.copytoclipboardSpan').on('click', function () {
+        $('.copytoclipboard').on('click', function () {
             var temp = $("<input>");
             $("body").append(temp);
             temp.val($(this).attr('data-href')).select();
+            temp.focus();
             document.execCommand("copy");
             temp.remove();
             return notif({
@@ -25,63 +25,7 @@ $this->registerJs($script);
 ?>
 
 <style>
-    .copytoclipboardModal {
-        display: none;
-    }
-
-    .modal-content .copytoclipboardModal {
-        display: list-item;
-    }
-
-    .modal-content .copytoclipboard {
-        display: none;
+    .copytoclipboard {
+        display: list-item !important;
     }
 </style>
-
-
-<script>
-    function copyToClipboardModal() {
-        $('.copytoclipboardModalSpan').on('click', function() {
-            text = $(this).attr('data-href');
-            if (window.location.protocol !== 'https:') {
-                console.warn('Clipboard API requires HTTPS');
-                // return;
-            }
-
-            if (navigator.clipboard && navigator.clipboard.writeText) {
-                navigator.clipboard.writeText(text).then(function() {
-                    return notif({
-                        type: 'success',
-                        msg: 'Link Copied',
-                        position: "right",
-                    });
-                }).catch(function(error) {
-                    console.error('Failed to copy text: ', error);
-                    copyToClipboardFallback(text);
-                });
-            } else {
-                copyToClipboardFallback(text);
-            }
-        });
-    }
-
-    function copyToClipboardFallback(text) {
-        var textArea = document.createElement("textarea");
-        textArea.value = text;
-        document.body.appendChild(textArea);
-        textArea.select();
-        try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Fallback: Copying text command was ' + msg);
-            return notif({
-                type: 'success',
-                msg: 'Link Copied',
-                position: "right",
-            });
-        } catch (err) {
-            console.error('Fallback: Oops, unable to copy', err);
-        }
-        document.body.removeChild(textArea);
-    }
-</script>
