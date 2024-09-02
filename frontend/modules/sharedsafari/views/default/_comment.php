e8abbb22e19d6a8251964286343a7c259ad62e22
Akash Kumar <akashprajapatiak05@gmail.com>
2024-09-02 16:05:55 +0530

    identity set

--- frontend/modules/sharedsafari/views/default/_comment.php
+++ frontend/modules/sharedsafari/views/default/_comment.php
@@ -50,7 +50,7 @@ use yii\helpers\Url;
                         <div class="one_box position-relative">
                             <div class="objec-flgs">
 
-                                <?php if ($comments->user) {
+                                <?php if ($comments->user && Yii::$app->user->identity) {
                                     $share_safari_intrested = ShareSafariIntrested::find()->where(['user_id' => Yii::$app->user->identity->id, 'share_safari_id' => $share_safari->id, 'status' => 1])->limit(1)->one();
                                     if ($share_safari_intrested  && $share_safari_intrested->user_id != $comments->user_id) { ?>
                                         <img src="<?= $this->params['baseurl'] ?>/img/Share-Safari/flag.png" alt="" class="flagBtn" value="<?= Url::toRoute(['/sharedsafari/default/flag', 'slug' => $share_safari->slug, 'park_id' => $share_safari->park_id, 'share_safari_comment_id' => $comments->id]) ?>">
@@ -110,7 +110,7 @@ use yii\helpers\Url;
                                                             <p><?= $reply->comment ?></p>
                                                         </div>
 
-                                                        <?php if ($reply->user) {
+                                                        <?php if ($reply->user && Yii::$app->user->identity) {
                                                             $share_safari_intrested = ShareSafariIntrested::find()->where(['user_id' => Yii::$app->user->identity->id, 'share_safari_id' => $share_safari->id, 'status' => 1])->limit(1)->one();
                                                             if ($share_safari_intrested && $share_safari_intrested->user_id != $reply->user_id) {
                                                         ?>
