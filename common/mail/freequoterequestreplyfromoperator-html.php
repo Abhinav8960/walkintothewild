f64382a8b40c055dbbf240e70531dbbccdf5f9a9
shokeenn <shokeen.triline@gmail.com>
2024-09-05 20:30:07 +0530

    push code

--- common/mail/freequoterequestreplyfromoperator-html.php
+++ common/mail/freequoterequestreplyfromoperator-html.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
     <p><img src="https://www.walkintothewild.in/img/logo.png"></p>
     <p>Hi <?= Html::encode($reply_to) ?>,</p>
     <p>Good news! You’ve received a reply from <?= Html::encode($reply_by) ?> regarding your quote request for <?= Html::encode($park_package_name) ?>. Please check your inbox to view the details and continue planning your adventure.</p>
-    <a style="background:#128A00;border:0; padding:8px 20px;color:#fff;text-decoration:none;" href="https://www.walkintothewild.in/<?= $chat_url ?>">Check Inbox</a>
+    <a style="background:#128A00;border:0; padding:8px 20px;color:#fff;text-decoration:none;" href="https://www.walkintothewild.in<?= $chat_url ?>">Check Inbox</a>
     <p>Best regards,</p>
     <p>Team Walk into the Wild</p>
 </div>
