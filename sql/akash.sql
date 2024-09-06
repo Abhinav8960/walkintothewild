65c6b786d76fab974a83d2117e3e21f9f17c67c9
Akash Kumar <akashprajapatiak05@gmail.com>
2024-09-06 17:35:14 +0530

    col mb-3 remove

--- sql/akash.sql
+++ sql/akash.sql
@@ -57,4 +57,4 @@ ALTER TABLE `package_comment`
   ALTER TABLE `share_safari_comment` ADD `is_deleted` INT NULL DEFAULT '0' AFTER `flaged`;
   ALTER TABLE `safari_operator_rating` ADD `is_deleted` INT NULL DEFAULT '0' AFTER `flaged`;
 
-  ALTER TABLE `article` ADD `user_status` INT NULL DEFAULT '0' AFTER `sequence`;
+  -- ALTER TABLE `article` ADD `user_status` INT NULL DEFAULT '0' AFTER `sequence`;
