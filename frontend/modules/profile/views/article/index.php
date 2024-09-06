37c7a2bd4045e5f2e66c5ba53e178315794a6d99
Akash Kumar <akashprajapatiak05@gmail.com>
2024-09-06 12:24:07 +0530

    user article done

--- frontend/modules/profile/views/article/index.php
+++ frontend/modules/profile/views/article/index.php
@@ -44,7 +44,16 @@ $this->params['title'] = $this->title;
                                                             </figure>
                                                         </div>
                                                         <div class="lower-content">
+                                                            <ul class="artical-info ">
+                                                                <li><i class="fa-solid fa-user"></i><a href="<?= Url::toRoute(['/profile/default/index', 'user_handle' => $user->user_handle]) ?>"><?= isset($article->articleAuthor) ? $article->articleAuthor->author_name : '' ?></a></li>
+
+                                                            </ul>
                                                             <h3><a href="<?= Url::toRoute(['view', 'slug' => $article->slug, 'user_handle' => $user->user_handle]) ?>"><?= $article->title ?> </a></h3>
+                                                            <div class="artical-info justify-content-center">
+                                                                <a href="<?= Url::toRoute(['/profile/article/view', 'slug' => $article->slug, 'user_handle' => $user->user_handle, '#' => 'comment-wrapper-section']) ?>" style="color: #9C9C9C;"><img src="<?= $this->params['baseurl'] ?>/img/comments.png" alt=""> <?= $article->getArticlecomments()->where(['status' => 1])->count() ?> Comments</a>
+
+                                                                <span style="color: #9C9C9C;"><i class="fa-solid fa-calendar-days me-1" style="color:#f9d600;"></i><?= date('M d, Y', strtotime($article->article_date)) ?></span>
+                                                            </div>
 
 
                                                         </div>
--- frontend/modules/profile/views/article/view.php
+++ frontend/modules/profile/views/article/view.php
@@ -53,7 +53,8 @@ $this->params['title'] = $this->title;
 
                                         <div class="author_wrapper pt-2">
                                             <ul class="artical-info flex-wrap">
-                                                <li><i class="fa-solid fa-user"></i><?= isset($article->articleAuthor) ? $article->articleAuthor->author_name : '' ?></li>
+                                                <li><i class="fa-solid fa-user"></i><a href="<?= Url::toRoute(['/profile/default/index', 'user_handle' => $user->user_handle]) ?>"><?= isset($article->articleAuthor) ? $article->articleAuthor->author_name : '' ?></a></li>
+                                                <li><img src="<?= $this->params['baseurl'] ?>/img/comments.png" alt=""><a href="#comment-wrapper-section"><?= $article->getArticlecomments()->where(['status' => 1, 'is_deleted' => 0])->count() ?> Comments</a></li>
                                                 <li><i class="fa-solid fa-calendar-days"></i><?= date('M d, Y', strtotime($article->article_date)) ?></li>
 
                                             </ul>
