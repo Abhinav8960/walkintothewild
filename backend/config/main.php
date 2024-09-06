3adcf8c31fc1fba7c96e5cb3062f6aa790c1581b
shokeenn <shokeen.triline@gmail.com>
2024-09-06 17:08:31 +0530

    changes in config files

--- backend/config/main.php
+++ backend/config/main.php
@@ -82,26 +82,6 @@ return [
             'site_key'   => isset($_SERVER['GOOGLE_CAPTCHA_SITE_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SITE_KEY'] : '6LdlvuYpAAAAAK2nW4xcNThJOMxVl2S6cGKqVJ9C',
             'secret_key' => isset($_SERVER['GOOGLE_CAPTCHA_SECRET_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SECRET_KEY'] : '6LdlvuYpAAAAABTlzZZ2dSAH3BhHL9WkxG7gfyUi',
         ],
-        'authClientCollection' => [
-            'class' => 'yii\authclient\Collection',
-            'clients' => [
-                'google' => [
-                    'class' => 'yii\authclient\clients\Google',
-                    // 'clientId' => '500526108747-sat0quehomhc59pogk8mm50ch2id5kp7.apps.googleusercontent.com',
-                    // 'clientSecret' => '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',
-                    // 'returnUrl' => 'http://admin.walkintothewild.io/site/auth?authclient=google',
-                    'clientId' => '631766851704-lbmn5e7lqhp6gnhf1jnumdrelbg5b00k.apps.googleusercontent.com',
-                    'clientSecret' => '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',
-                    'returnUrl' => 'https://staging-manage.walkintothewild.in/site/auth?authclient=google',
-                ],
-                // 'facebook' => [
-                //     'class' => 'yii\authclient\clients\Facebook',
-                //     'clientId' => 'facebook_client_id',
-                //     'clientSecret' => 'facebook_client_secret',
-                // ],
-                // etc.
-            ],
-        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
@@ -135,62 +115,10 @@ return [
                 }
             },
         ],
-
-        // 'mailer' => [
-        //     'class' => \yii\symfonymailer\Mailer::class,
-        //     'viewPath' => '@app/mail',
-        //     // send all mails to a file by default.
-        //     'useFileTransport' => true,
-        // ],
-        // 'mailer' => [
-        //     'class' => 'yii\swiftmailer\Mailer',
-        //     'useFileTransport' => false,
-        //     'transport' => [
-        //         'class' => 'Swift_SmtpTransport',
-        //         'host' => 'smtp.gmail.com',
-        //         'username' => 'smritipal2201@gmail.com',
-        //         'password' => 'ybsaphazhjtlakau',
-        //         'port' => '587',
-        //         'encryption' => 'tls',
-        //     ]
-        // ],
-
-
-        'mailer' => [
-            // 'class' => 'yii\swiftmailer\Mailer',
-            // 'class' => 'yii\symfonymailer\Mailer',
-            'class' => 'common\Mailer\Mailer',
-            // 'class' => 'apps\Mailer\AWSCustomMailer',
-            // 'transportFactory' => apps\Mailer\MailerTransportFactory::class,
-            'useFileTransport' => false,
-            'transport' => [
-                // 'class' => 'Swift_SmtpTransport',
-                // 'class' => 'Swift_AWSTransport',
-                // 'scheme' => 'smtps',
-                // 'host' => 'email-smtp.ap-south-1.amazonaws.com', // amazon smtp host 
-                // 'username' => 'AKIAX7HOT7POHICMCIWC', // ses user username
-                // 'password' => 'BK+0TXPP/u/jirG56C7E99hnkHm0liK6cvYYXrhfpadI', // ses user password
-                // 'port' => 465,
-                // 'encryption' => 'tls',
-                // 'dsn' => 'ses+smtp://AKIAX7HOT7POMUCN434G:urlencode(sOplW2xa1TCJ7+BhMMw1a8PwNYe8+oBj3zOqWY38)@email-smtp.ap-south-1.amazonaws.com?region=ap-south-1',
-                'dsn' => 'ses+api://AKIAX7HOT7PONBYCE4L3:kQQEyuej8UzSNMHEMBRGDOB2lzqj17zBRcvuMu4E@default?region=ap-south-1',
-                // 'dsn' => 'ses+api://AKIAX7HOT7POC2MAKZFN:OplW2xa1TCJ7+BhMMw1a8PwNYe8+oBj3zOqWY38@default?region=ap-south-1',
-                // 'dsn' => 'ses+api://AKIAX7HOT7POCAH4YAUW:fX+bZbSoAl2mUvTn5qldDzp+X9l5OyHmL3m1Vv5c@default?region=ap-south-1',
-
-
-
-
-
-
-            ],
-            'viewPath' => '@common/mail',
-
-        ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-
                 '/aws-mailer-notification' => '/aws-mailer-notification/index'
             ],
         ],
--- common/config/main.php
+++ common/config/main.php
@@ -6,8 +6,51 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\Google',
+                    'clientId' => '631766851704-lbmn5e7lqhp6gnhf1jnumdrelbg5b00k.apps.googleusercontent.com',
+                    'clientSecret' => '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',
+                ],
+            ],
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
+        ],
+        'db' => [
+            'class' => \yii\db\Connection::class,
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default.
+            'useFileTransport' => true,
+            // You have to set
+            //
+            // 'useFileTransport' => false,
+            //
+            // and configure a transport for the mailer to send real emails.
+            //
+            // SMTP server example:
+            //    'transport' => [
+            //        'scheme' => 'smtps',
+            //        'host' => '',
+            //        'username' => '',
+            //        'password' => '',
+            //        'port' => 465,
+            //        'dsn' => 'native://default',
+            //    ],
+            //
+            // DSN example:
+            //    'transport' => [
+            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
+            //    ],
+            //
+            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
+            // Or if you use a 3rd party service, see:
+            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
         ],
     ],
 ];
