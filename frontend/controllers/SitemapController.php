fbd9b879fccfaee80eadd8ce4b51b309c0a92618
SMriti <smritipal2201@gmail.com>
2024-09-06 19:08:29 +0530

    changes done for free quote request

--- frontend/controllers/SitemapController.php
+++ frontend/controllers/SitemapController.php
@@ -15,13 +15,13 @@ class SitemapController extends FrontendBaseController
    * Renders the index view for the module
    * @return string
    */
-  public function actionIndex()
-  {
-    $filepath = \Yii::$app->params['datapath'] . "/" . "sitemap/" . "sitemap_index.xml";
-    \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
-    \Yii::$app->response->headers->add('Content-Type', 'text/xml');
-    return file_get_contents($filepath);
-  }
+  // public function actionIndex()
+  // {
+  //   $filepath = \Yii::$app->params['datapath'] . "/" . "sitemap/" . "sitemap_index.xml";
+  //   \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
+  //   \Yii::$app->response->headers->add('Content-Type', 'text/xml');
+  //   return file_get_contents($filepath);
+  // }
 
   public function actionArticle()
   {
