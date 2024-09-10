9133753b9441b16de68f0186bb5153999094bdac
SMriti <smritipal2201@gmail.com>
2024-09-10 14:15:53 +0530

    changes done for package icons

--- frontend/modules/package/views/default/view.php
+++ frontend/modules/package/views/default/view.php
@@ -164,7 +164,7 @@ $banner = Banner::find()->where(['status' => 1, 'page_id' => $page_constant])->l
                                 <div class="col-12 col-sm-6 mb-3">
                                     <div class="safridetails_form d-flex gap-3 ">
                                         <div class="iconImg">
-                                            <img src="<?= $this->params['baseurl'] ?>/img/camera.png" alt="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Photography Special">
+                                            <img src="<?= $this->params['baseurl'] ?>/img/camera.png" alt="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Camera Fee">
                                         </div>
                                         <div class="text-form">
                                             <p class="mb-0"><?php
