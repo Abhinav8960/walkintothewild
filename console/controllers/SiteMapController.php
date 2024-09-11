c87466a032969b3b989dcead0cdb055b18e5022b
SMriti <smritipal2201@gmail.com>
2024-09-11 18:25:55 +0530

    changes done for sitemap

--- console/controllers/SiteMapController.php
+++ console/controllers/SiteMapController.php
@@ -29,106 +29,106 @@ class SiteMapController extends Controller
   {
     $script_start1 = microtime(true);
     echo "Script 1 start time: " . $script_start1 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-pages1'); //run the Action
     $script_end1 = microtime(true);
     $executionTime = $script_end1 - $script_start1;
     echo "Script 1 end time: " . $script_end1 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 1 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
 
     $script_start2 = microtime(true);
     echo "Script 2 start time: " . $script_start2 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-pages2'); //run the Action
     $script_end2 = microtime(true);
     $executionTime = $script_end2 - $script_start2;
     echo "Script 2 end time: " . $script_end2 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 2 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     // $script_start3 = microtime(true);
-    // echo "<pre>";
     // echo "Script 3 start time: " . $script_start3 . " seconds";
+    // echo "\n";
     // \Yii::$app->runAction('generate-site-pages/site-pages3'); //run the Action to get join/unjoin
     // $script_end3 = microtime(true);
     // $executionTime = $script_end3 - $script_start3;
     // echo "Script 3 end time: " . $script_end3 . " seconds";
-    // echo "<pre>";
+    // echo "\n";
     // echo "Script 3 execution time: " . $executionTime . " seconds";
-    // echo "<pre>";
+    // echo "\n";
 
 
 
     $script_start4 = microtime(true);
-    echo "<pre>";
     echo "Script 4 start time: " . $script_start4 . " seconds";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-pages4'); //run the Action to get_monthly_package_site_pages(),get_monthly_shared_safari_site_pages(),get_operator_tabs_site_pages();
     $script_end4 = microtime(true);
     $executionTime = $script_end4 - $script_start4;
     echo "Script 4 end time: " . $script_end4 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 4 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     $script_start5 = microtime(true);
     echo "Script 5 start time: " . $script_start5 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-pages1'); //run the Action get park,share safari,package
     $script_end5 = microtime(true);
     $executionTime = $script_end5 - $script_start5;
     echo "Script 5 end time: " . $script_end5 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 5 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     $script_start6 = microtime(true);
-    echo "<pre>";
     echo "Script 6 start time: " . $script_start6 . " seconds";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-pages6'); //run the Action User Follow
     $script_end6 = microtime(true);
     $executionTime = $script_end6 - $script_start6;
     echo "Script 6 end time: " . $script_end6 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 6 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     $script_start7 = microtime(true);
     echo "Script 7 start time: " . $script_start7 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-page7'); //run the Action Operator Follow
     $script_end7 = microtime(true);
     $executionTime = $script_end7 - $script_start7;
     echo "Script 7 end time: " . $script_end7 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 7 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     $script_start8 = microtime(true);
     echo "Script 8 start time: " . $script_start8 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('generate-site-pages/site-page8'); //run the Action Whishlist
     $script_end8 = microtime(true);
     $executionTime = $script_end8 - $script_start8;
     echo "Script 8 end time: " . $script_end8 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 8 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
@@ -142,40 +142,40 @@ class SiteMapController extends Controller
 
     $script_start10 = microtime(true);
     echo "Script 10 start time: " . $script_start10 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('create-site-xml/site-pages-counter'); //run the Action Create true page counter
     $script_end10 = microtime(true);
     $executionTime = $script_end10 - $script_start10;
     echo "Script 10 end time: " . $script_end10 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 10 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
     $script_start11 = microtime(true);
     echo "Script 11 start time: " . $script_start11 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('create-site-xml/non-site-pages-counter'); //run the Action Create false page counter
     $script_end11 = microtime(true);
     $executionTime = $script_end11 - $script_start11;
     echo "Script 11 end time: " . $script_end11 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 11 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
 
 
 
 
     $script_start12 = microtime(true);
     echo "Script 12 start time: " . $script_start12 . " seconds";
-    echo "<pre>";
+    echo "\n";
     \Yii::$app->runAction('create-site-xml/index'); //run the Action Create XML
     $script_end12 = microtime(true);
     $executionTime = $script_end12 - $script_start12;
     echo "Script 12 end time: " . $script_end12 . " seconds";
-    echo "<pre>";
+    echo "\n";
     echo "Script 12 execution time: " . $executionTime . " seconds";
-    echo "<pre>";
+    echo "\n";
   }
 }
