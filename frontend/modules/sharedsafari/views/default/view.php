672d6e6a932f4c5e38d35bae9c2bd127a7cf1830
Vikas Lat-3500 <vikas.k.c@gmail.com>
2024-09-14 18:07:04 +0530

    Google Ads

--- frontend/modules/sharedsafari/views/default/view.php
+++ frontend/modules/sharedsafari/views/default/view.php
@@ -285,12 +285,20 @@ $this->params['title'] = $this->title; ?>
                     </div>
                 </div>
 
-                <div class="advertisment d-lg-block d-none mt-5">
-                    <div class="google-ad300  mb-5">
-
+                <div class="advertisment pt-md-2 pt-5" >
+                    <div class="google-ad-box  mb-5" style="border:none">
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-6116324330184807"
+                            data-ad-slot="9287923659"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                     </div>
                 </div>
-                <div class="advertisment d-lg-block d-none ">
+                <div class="advertisment d-lg-block d-none " style="display: none">
                     <div class="google-add600hight  mb-5">
 
                     </div>
