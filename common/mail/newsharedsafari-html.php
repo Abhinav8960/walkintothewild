5ade1a1923a80ad2c3c7e767bf2c18524bdf2a06
Akash Kumar <akashprajapatiak05@gmail.com>
2024-09-18 12:57:41 +0530

    shared safari created mail

---
+++ common/mail/newsharedsafari-html.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<div class="contact-form" style="font-family: Arial, sans-serif; font-size: 14px; color: #333; padding: 20px; border: 1px solid #ddd; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+    <p style="font-weight: bold; margin-bottom: 10px;">Hi Admin,</p>
+    <p style="margin-bottom: 20px;"><?= $username && isset($username) ? $username : '' ?> created a new Shared Safari.</p>
+    <br>
+    <table style="width: 100%; border-collapse: collapse;">
+        <tbody>
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;"> Name</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari && isset($shared_safari['share_safari_title']) ? $shared_safari['share_safari_title'] : '' ?></td>
+            </tr>
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;">Number of Safari</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari && isset($shared_safari['no_of_safari']) ? $shared_safari['no_of_safari'] : '' ?></td>
+            </tr>
+
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;">Start Date</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari && isset($shared_safari['start_date']) ? $shared_safari['start_date'] : '' ?></td>
+            </tr>
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;">End Date</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari && isset($shared_safari['end_date']) ? $shared_safari['end_date'] : '' ?></td>
+            </tr>
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;">Total Seat</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari && isset($shared_safari['total_seat']) ? $shared_safari['total_seat'] : '' ?></td>
+            </tr>
+            <tr>
+                <th style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ddd; text-align: left;">Link</th>
+                <td style="padding: 10px; border: 1px solid #ddd;"><?= $shared_safari_url && isset($shared_safari_url) ? $shared_safari_url : '' ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <p style="margin-top: 2% !important; font-size: 12px; color: #666;">Thank you!</p>
+    <p style="font-size: 12px; color: #666;">Best regards,</p>
+    <p style="font-size: 12px; color: #666; font-weight: bold;">Team Walk into the Wild</p>
+</div>
