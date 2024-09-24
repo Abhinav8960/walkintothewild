3210e3abbd3331fb74643c69e6166cef135b7afe
Akash Kumar <akashprajapatiak05@gmail.com>
2024-09-24 12:48:57 +0530

    master of author article

---
+++ backend/modules/cms/controllers/MasterArticleAuthorController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace backend\modules\cms\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use common\models\cms\article\MasterArticleAuthor;
+use common\models\cms\article\form\MasterArticleAuthorForm;
+use common\models\cms\article\MasterArticleAuthorSearch;
+use common\models\cms\article\MasterArticleTag;
+
+/**
+ * MasterArticleAuthor Controller for the `article` module
+ */
+class MasterArticleAuthorController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new MasterArticleAuthorSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Create Article Author
+     * 
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new MasterArticleAuthorForm();
+        $model->status = MasterArticleAuthor::STATUS_ACTIVE;
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post())) {
+                if ($model->validate()) {
+                    $model->initializeForm();
+                    if ($model->master_article_author_model->save()) {
+                        \Yii::$app->session->setFlash('success', 'Data Submitted Successfully');
+                        return $this->redirect(['/cms/master-article-author/index']);
+                    }
+                }
+            }
+        } else {
+            $model->master_article_author_model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+
+    /**
+     * Updates an existing Article Author model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $master_article_author_model = $this->findModel($id);
+        $model = new MasterArticleAuthorForm($master_article_author_model);
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post())) {
+                if ($model->validate()) {
+                    $model->initializeForm();
+                    if ($model->master_article_author_model->save()) {
+                        \Yii::$app->session->setFlash('success', 'Data Updated Successfully');
+                        return $this->redirect(['/cms/master-article-author/index']);
+                    }
+                }
+            }
+        } else {
+            $model->master_article_author_model->loadDefaultValues();
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+
+
+
+    /**
+     * Deletes an existing MasterArticleAuthor model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $model = $this->findModel($id);
+        $model->status = MasterArticleAuthor::STATUS_DELETE;
+        $model->save();
+        \Yii::$app->session->setFlash('success', 'Data Updated Successfully');
+        return $this->redirect(Yii::$app->request->referrer);
+    }
+
+    /**
+     * Finds the MasterArticleAuthor model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return MasterArticleAuthor the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = MasterArticleAuthor::findOne(['id' => $id, 'status' => [MasterArticleAuthor::STATUS_ACTIVE, MasterArticleAuthor::STATUS_SUSPEND]])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ backend/modules/cms/views/master-article-author/index.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+
+$this->title = 'Master Article Author';
+$this->params['breadcrumbs_home_url'] = '/cms/master-article-author';
+$this->params['breadcrumbs'][] =  ['label' => 'CMS', 'url' => '#'];
+$this->params['breadcrumbs'][] = $this->title;
+$this->params['title'] = $this->title;
+$this->params['buttons'][] = Html::a('+ Create', ['create'], ['class' => 'btn btn-orange ', 'title' => 'Create']);
+?>
+<div class="card">
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'class' => 'yii\grid\SerialColumn',
+                        'contentOptions' => ['style' => 'width: 5%;'],
+                    ],
+                    [
+                        'label' => 'Author Name',
+                        'contentOptions' => ['style' => 'width: 10%;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return $model->name;
+                        }
+                    ],
+
+                    [
+                        'label' => 'Status',
+                        'contentOptions' => ['style' => 'width: 10%; text-align: center;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return $model->newstatuslabel;
+                        }
+                    ],
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'header' => "Actions",
+                        'contentOptions' => ['style' => 'width: 15%; text-align: center;'],
+                        'template' => '{update}&nbsp;&nbsp;{delete}',
+                        'buttons' => [
+
+                            'update' => function ($url, $model) {
+                                return  Html::a('<img src="' . $this->params['baseurl'] . '/img/update.png" alt="" width="25" height="25">
+                                ', ['update', 'id' => $model->id], [
+                                    'class' => 'btn p-0 change-menuicon',
+                                    'name' => 'Update',
+
+                                ]);
+                            },
+                            'delete' => function ($url, $model) {
+                                return  Html::a('<img src="' . $this->params['baseurl'] . '/img/delete.png" alt="" width="25" height="25">', ['delete', 'id' => $model->id], [
+                                    'class' => 'btn p-0 change-menuicon',
+                                    'name' => 'Delete',
+                                    'data' => [
+                                        'confirm' => 'Are you sure you want to delete  ' . $model->name . '?',
+                                        'method' => 'post',
+                                    ],
+                                ]);
+                            },
+                        ]
+                    ],
+                ],
+            ]); ?>
+        </div>
+    </div>
+</div>
