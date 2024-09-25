32aaca45a4bbd4000542008c859a46f00cb6f0ff
Anurag <mranurag101@gmail.com>
2024-09-25 14:03:15 +0530

    changes

---
+++ common/traits/TableInfoTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace common\traits;
+
+
+
+/**
+ * Table Info Trait
+ */
+trait TableInfoTrait
+{
+
+
+    
+    public function updatetableinfor($totalcount, $lastupdatedtime = date("Y-m-d H:i:s"))
+    {
+        $filepath = \Yii::$app->params['table_info_location'];
+
+        
+    }
+}
