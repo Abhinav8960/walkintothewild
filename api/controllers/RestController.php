df7f8439db0be316fddffe65af67ceb69691db37
Akash Kumar <akashprajapatiak05@gmail.com>
2024-10-11 11:10:51 +0530

    allowed domain set

--- api/controllers/RestController.php
+++ api/controllers/RestController.php
@@ -56,7 +56,6 @@ class RestController extends Controller
             '*',                        // star allows all domains
             'http://localhost:3000',
             'http://localhost:8081',
-            'https://spider.triline.in',
         ];
     }
 
