acaa6946805c2f73da3b00637c2fc4db490e4e6a
Akash Kumar <akashprajapatiak05@gmail.com>
2024-10-21 17:10:11 +0530

    now make component of firebase

---
+++ common/components/FirebaseMessaging.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace  common\components;
+
+use Google\Client;
+use Yii;
+
+class FirebaseMessaging
+{
+    private $serviceAccountPath;
+    private $projectId;
+
+    public function __construct($projectId)
+    {
+        $this->serviceAccountPath = Yii::getAlias('@common/config/service.json');
+        $this->projectId = $projectId;
+    }
+
+    public function getAccessToken()
+    {
+        $client = new Client();
+        $client->setAuthConfig($this->serviceAccountPath);
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client->useApplicationDefaultCredentials();
+        $token = $client->fetchAccessTokenWithAssertion();
+        return $token['access_token'];
+    }
+
+    public function sendMessage($accessToken, $message)
+    {
+        $url = 'https://fcm.googleapis.com/v1/projects/' . $this->projectId . '/messages:send';
+        $headers = [
+            'Authorization: Bearer ' . $accessToken,
+            'Content-Type: application/json',
+        ];
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['message' => $message]));
+        
+        $response = curl_exec($ch);
+        
+        if ($response === false) {
+            throw new \Exception('Curl error: ' . curl_error($ch));
+        }
+        
+        curl_close($ch);
+        return json_decode($response, true);
+    }
+}
--- frontend/controllers/NotificationController.php
+++ frontend/controllers/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\controllers;
 
+use common\components\FirebaseMessaging;
 use common\Helper\FirebaseNotificationHelper;
 use Yii;
 use yii\web\Controller;
@@ -12,26 +13,48 @@ use frontend\models\ContactForm;
  */
 class NotificationController extends FrontendBaseController
 {
-    public function actionSend()
+    // public function actionSend()
+    // {
+    //     $token = 'cZM3VdFASAScms7KuxpAcf:APA91bFsL5_4XNKGWAaA47ZuLaTiHnbXTTm2SUsb7Y2Pb26-FhKzf18xqTa48plEZIX8f27jj5StyWqqL0EkUWU6Scn5vUthTS3QIdeA_7b8Dd7QFXOmxCMJfoY3xRHpbMtxXQXAw7uA'; // Replace with the recipient device token
+    //     $title = 'Test Notification';
+    //     $body = 'This Test Notification !';
+    //     $notification = [
+    //         'title' => $title,
+    //         'body' => $body,
+    //     ];
+
+    //     $service = new FirebaseNotificationHelper([
+    //         'authKey' => Yii::$app->params['firebase']['private_key'],
+    //         'projectId' => Yii::$app->params['firebase']['project_id'],
+    //     ]);
+
+    //     try {
+    //         $response = $service->sendNotification($token, $notification);
+    //         return $this->asJson($response);
+    //     } catch (\Exception $e) {
+    //         return $this->asJson(['error' => $e->getMessage()]);
+    //     }
+    // }
+
+    public function actionSendNotification()
     {
-        $token = 'cZM3VdFASAScms7KuxpAcf:APA91bFsL5_4XNKGWAaA47ZuLaTiHnbXTTm2SUsb7Y2Pb26-FhKzf18xqTa48plEZIX8f27jj5StyWqqL0EkUWU6Scn5vUthTS3QIdeA_7b8Dd7QFXOmxCMJfoY3xRHpbMtxXQXAw7uA'; // Replace with the recipient device token
-        $title = 'Test Notification';
-        $body = 'This Test Notification !';
-        $notification = [
-            'title' => $title,
-            'body' => $body,
+        $token = 'cZM3VdFASAScms7KuxpAcf:APA91bFsL5_4XNKGWAaA47ZuLaTiHnbXTTm2SUsb7Y2Pb26-FhKzf18xqTa48plEZIX8f27jj5StyWqqL0EkUWU6Scn5vUthTS3QIdeA_7b8Dd7QFXOmxCMJfoY3xRHpbMtxXQXAw7uA';
+        $projectId = 'walkintothewild-24344';
+        $firebaseMessaging = new FirebaseMessaging($projectId);
+        $message = [
+            'token' => $token,
+            'notification' => [
+                'title' => 'Check New Way',
+                'body' => 'Dynamic Way',
+            ],
         ];
 
-        $service = new FirebaseNotificationHelper([
-            'authKey' => Yii::$app->params['firebase']['private_key'],
-            'projectId' => Yii::$app->params['firebase']['project_id'],
-        ]);
-
         try {
-            $response = $service->sendNotification($token, $notification);
-            return $this->asJson($response);
+            $accessToken = $firebaseMessaging->getAccessToken();
+            $response = $firebaseMessaging->sendMessage($accessToken, $message);
+            return $this->asJson(['status' => 'success', 'response' => $response]);
         } catch (\Exception $e) {
-            return $this->asJson(['error' => $e->getMessage()]);
+            return $this->asJson(['status' => 'error', 'message' => $e->getMessage()]);
         }
     }
 }
