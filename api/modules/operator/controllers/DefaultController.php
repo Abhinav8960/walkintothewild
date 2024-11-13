a5fcf3ee9ed6ea211e8f031177aa183f2650ec73
Akash Kumar <akashprajapatiak05@gmail.com>
2024-11-13 13:26:09 +0530

    quotes request

--- api/modules/operator/controllers/DefaultController.php
+++ api/modules/operator/controllers/DefaultController.php
@@ -9,6 +9,7 @@ use api\behaviours\Verbcheck;
 use api\controllers\RestController;
 use api\models\operator\SafariOperator;
 use api\models\operator\SafariOperatorSearch;
+use frontend\models\OperatorQuoteForm;
 use yii\filters\AccessControl;
 
 /**
@@ -62,4 +63,26 @@ class DefaultController extends RestController
         return $this->dataProviderSender($searchModel, $rootIndexName = 0, $additionalSearchQueryParams = [], $singleRecord = true);
         // return $this->dataSender($operator, $rootIndexName = "Operator");
     }
+
+
+    // public function actionQuotesrequest($slug)
+    // {
+    //     $operator = SafariOperator::find()->where(['status' => SafariOperator::STATUS_ACTIVE, 'slug' => $slug])->limit(1)->one();
+    //     if (!$operator) {
+    //         return Yii::$app->api->sendResponse($data = [], ['message' => "Operator Not Found!!!"]);
+    //     }
+    //     $model = new OperatorQuoteForm();
+    //     if ($this->userinfo) {
+    //         $model->email = $this->userinfo->email;
+    //         $model->full_name = $this->userinfo->name;
+    //         $model->phone_no = $this->userinfo->mobile_no;
+    //     }
+    //     $model->attributes = $this->request;
+    //     if ($model->validate()) {
+    //         if ($operator_quote = $model->request($operator)) {
+    //             // FrontendNotificationHelper::operatorNewQuote($operator, $operator_quote, Yii::$app->user->identity);
+    //         }
+    //         return Yii::$app->api->sendResponse($data = [$model->attributes], ['message' => 'Quote request sent!']);
+    //     }
+    // }
 }
