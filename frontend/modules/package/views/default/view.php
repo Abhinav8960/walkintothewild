d38869d7af3793554902a9d6b02e3dc9f91107f1
Akash Kumar <akashprajapatiak05@gmail.com>
2024-11-26 18:36:34 +0530

    remove gallary

--- frontend/modules/package/views/default/view.php
+++ frontend/modules/package/views/default/view.php
@@ -441,7 +441,7 @@ $banner = Banner::find()->where(['status' => 1, 'page_id' => $page_constant])->l
                     </div>
                 <?php } ?>
 
-                <?php if ($package->packagegallery) {
+                <!-- <?php if ($package->packagegallery) {
                     $galleries = $package->packagegallery;
                 ?>
                     <div class="request_quote photoGallry mb-4">
@@ -460,7 +460,7 @@ $banner = Banner::find()->where(['status' => 1, 'page_id' => $page_constant])->l
                             </div>
                         </div>
                     </div>
-                <?php } ?>
+                <?php } ?> -->
 
                 <div class="">
                     <?php
