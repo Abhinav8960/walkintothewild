6978a7a0c2cabffe46109dd46ef961ba532ccd5d
Akash Kumar <akashprajapatiak05@gmail.com>
2025-01-10 13:38:05 +0530

    API : Operator

--- api/modules/manage/controllers/DefaultController.php
+++ api/modules/manage/controllers/DefaultController.php
@@ -62,6 +62,7 @@ class DefaultController extends RestController
      */
     public function actionIndex()
     {
+        $this->layout = \common\interfaces\NewStatusInterface::OPERATOR_API_LAYOUT_FULL;
         $safari_operator = $this->module->operatormodel();
         return Yii::$app->api->sendResponse($data = $safari_operator);
     }
@@ -174,7 +175,7 @@ class DefaultController extends RestController
             return Yii::$app->api->sendResponse($data = ['status' => 0], ['message' => "You are not operator"]);
         }
         $searchModel = new ShareSafariSearch();
-        return $this->dataProviderSender($searchModel, $rootIndexName = "ShareSafarilist", $additionalSearchQueryParams = [$safari_operator->id], $singleRecord = false, $paginationNeededAsPerQuery = 1,$searchfunction = "managesearch");
+        return $this->dataProviderSender($searchModel, $rootIndexName = "ShareSafarilist", $additionalSearchQueryParams = [$safari_operator->id], $singleRecord = false, $paginationNeededAsPerQuery = 1, $searchfunction = "managesearch");
     }
 
 
@@ -185,6 +186,6 @@ class DefaultController extends RestController
             return Yii::$app->api->sendResponse($data = ['status' => 0], ['message' => "You are not operator"]);
         }
         $searchModel = new PackageSearch();
-        return $this->dataProviderSender($searchModel, $rootIndexName = "Packagelist", $additionalSearchQueryParams = [$safari_operator->id], $singleRecord = false,$paginationNeededAsPerQuery = 1, $searchfunction = "managesearch");
+        return $this->dataProviderSender($searchModel, $rootIndexName = "Packagelist", $additionalSearchQueryParams = [$safari_operator->id], $singleRecord = false, $paginationNeededAsPerQuery = 1, $searchfunction = "managesearch");
     }
 }
