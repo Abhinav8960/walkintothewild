cffab2a522aee0f578989db0f38bed4a81c0e04d
Akash Kumar <akashprajapatiak05@gmail.com>
2025-03-07 13:28:44 +0530

    User Post Like Dislike

---
+++ api/models/posts/UserPostLike.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace api\models\posts;
+
+use Yii;
+
+class UserPostLike extends \common\models\postscomment\UserPostLike
+{
+    public function fields()
+    {
+        $fields = parent::fields();
+        $hold_fields = ['status', 'created_by', 'updated_by', 'created_at', 'updated_at'];
+        return array_diff($fields, $hold_fields);
+        return $fields;
+    }
+
+}
