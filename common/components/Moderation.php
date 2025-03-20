81fb753fe464075af687075cffdc1982701425ec
kamal <kamalkholiya1001@gmail.com>
2025-03-20 16:20:14 +0530

    image moderation

--- common/components/Moderation.php
+++ common/components/Moderation.php
@@ -4,7 +4,9 @@ namespace common\components;
 
 use common\models\moderation\form\ModerationForm;
 use common\models\moderation\ImageAlcohol;
+use common\models\moderation\ImageBrightness;
 use common\models\moderation\ImageColors;
+use common\models\moderation\ImageContrast;
 use common\models\moderation\ImageDestruction;
 use common\models\moderation\ImageFaces;
 use common\models\moderation\ImageGambling;
@@ -20,6 +22,7 @@ use common\models\moderation\ImageRecreationalDrug;
 use common\models\moderation\ImageRequest;
 use common\models\moderation\ImageScam;
 use common\models\moderation\ImageSelfHarm;
+use common\models\moderation\ImageSharpness;
 use common\models\moderation\ImageTobacco;
 use common\models\moderation\ImageType;
 use common\models\moderation\ImageViolence;
@@ -61,6 +64,8 @@ class Moderation extends Component
     // private $sightEngineUserId = "101632135"; // Anurag
     // private $sightEnginesecretId = "FRrzHTpHk7GBvY86HokP7MV884SbrRHu"; // Anurag
 
+    private $sightEngineUserId = "635987644"; // Kamal New
+    private $sightEnginesecretId = "XLnfEEohEagNUo6hUCGqfXL8ncmuQ5Lw"; // Kamal New
 
     private $models = [
         'nudity-2.1',
@@ -303,17 +308,32 @@ class Moderation extends Component
         $type_saved = ImageType::typeStore($feedback, $moderationId);
         $violence_saved = ImageViolence::voilenceStore($feedback, $moderationId);
         $weapon_saved = ImageWeapon::weaponStore($feedback, $moderationId);
-        $faced_saved = VideoFace::facestore($feedback, $moderationId);
+        $brightness_saved = ImageBrightness::brightnessStore($feedback, $moderationId);
+        $sharpness_saved = ImageSharpness::sharpnessStore($feedback, $moderationId);
+        $contrast_saved = ImageContrast::contrastStore($feedback, $moderationId);
 
         if (
             $nudity_saved && $offensive_saved && $gore_saved && $weapon_saved && $self_harm_saved && $face_saved && $scam_saved
-            && $tobacco_saved && $request_saved && $quality_saved && $violence_saved && $medical_saved
-            && $alcohol_saved && $gambling_saved && $money_saved && $color_saved && $type_saved && $destruction_saved
-            && $military_saved && $media_saved && $faced_saved
+            && $tobacco_saved && $request_saved && $quality_saved && $violence_saved && $medical_saved && $alcohol_saved
+            && $gambling_saved && $money_saved && $color_saved && $type_saved && $destruction_saved && $military_saved
+            && $media_saved && $contrast_saved && $sharpness_saved && $brightness_saved
         ) {
             echo "Image Data Stored Successfully";
         } else {
             exit("Error: Failed to store data");
         }
     }
+
+    // private function actionStoreImage($feedback, $moderation_type, $moderationId)
+    // {
+    //     $brightness_saved = ImageBrightness::brightnessStore($feedback, $moderationId);
+    //     $sharpness_saved = ImageSharpness::sharpnessStore($feedback, $moderationId);
+    //     $contrast_saved = ImageContrast::contrastStore($feedback, $moderationId);
+
+    //     if ($contrast_saved && $sharpness_saved && $brightness_saved) {
+    //         echo "Image Data Stored Successfully";
+    //     } else {
+    //         exit("Error: Failed to store data");
+    //     }
+    // }
 }
