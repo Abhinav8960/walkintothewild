3095a2474b667a8eb38b9db39c47bf401d26a8a2
Akash Kumar <akashprajapatiak05@gmail.com>
2025-03-20 16:30:36 +0530

    Url Short ner link

--- backend/widgets/views/sidebar.php
+++ backend/widgets/views/sidebar.php
@@ -1176,12 +1176,12 @@ $active_url = "/" . Yii::$app->requestedRoute;
 				<?php } ?>
 
 				<?php if (Yii::$app->user->identity->is_adminstrator || Yii::$app->user->identity->is_admin) : ?>
-					<!-- <li class="slide">
+					<li class="slide">
 						<a class="side-menu__item <?= in_array($active_url, array(
 														"/urlshortner",
 														"/urlshortner/default/index",
 													)) ? "active" : "" ?>" href="/urlshortner/default/index"><img src="<?= $this->params['baseurl'] ?>/img/carbon_workspace.png" alt="" width="25" height="25" class="navhover_icon"><span class="side-menu__label">Url Shortner</span></a>
-					</li> -->
+					</li>
 				<?php endif; ?>
 
 				<?php if (Yii::$app->user->identity->is_adminstrator || Yii::$app->user->identity->is_admin) : ?>
