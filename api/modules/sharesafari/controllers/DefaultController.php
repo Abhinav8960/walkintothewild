bf7b34a997b1785333763e356fc7ce8e183424db
Akash Kumar <akashprajapatiak05@gmail.com>
2025-04-14 12:04:20 +0530

    Profile Delete

--- api/modules/sharesafari/controllers/DefaultController.php
+++ api/modules/sharesafari/controllers/DefaultController.php
@@ -605,7 +605,7 @@ class DefaultController extends SafariController
                 $model->shared_safari_model->savehistory();
                 $model->UploadFiles($model->shared_safari_model->id);
                 /* All Joined User*/
-                $intrested_users = $shared_safari_model->getIntrested()->where(['share_safari_intrested.status' => 1])->all();
+                $intrested_users = $shared_safari_model->getIntrested()->joinWith('user')->andWhere(['user.status' => 10, 'share_safari_intrested.status' => 1])->all();
                 if ($intrested_users) {
                     foreach ($intrested_users as $intrest) {
                         $user = $intrest->user;
