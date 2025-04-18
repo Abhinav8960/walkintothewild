45a1588c9c2078c05e176589dde45009816aebc9
vikas <vikas@vikas-Latitude-5550>
2025-04-18 19:06:55 +0530

    Minor change

--- common/config/main.php
+++ common/config/main.php
@@ -3,6 +3,8 @@ return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        
+        
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
