7fb61a4786fd929b699f2fd5399acac9408251f3
Anurag <mranurag101@gmail.com>
2025-04-20 11:23:24 +0530

    change

---
+++ common/models/packageapproval/PackageDay.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace common\models\packageapproval;
+
+use Yii;
+
+/**
+ * This is the model class for table "package_day".
+ *
+ * @property int $id
+ * @property int $package_id
+ * @property int $day
+ * @property string|null $day_title
+ * @property string|null $day_description
+ * @property string|null $start_location
+ * @property string|null $end_location
+ * @property string|null $hotel_name
+ * @property string|null $day_image
+ * @property int|null $meal_lunch
+ * @property int|null $meal_breakfast
+ * @property int|null $meal_dinner
+ * @property string|null $day_activity
+ * @property string|null $day_accommodation
+ * @property string|null $day_note
+ * @property int|null $status
+ * @property int|null $created_at
+ * @property int|null $created_by
+ * @property int|null $updated_at
+ * @property int|null $updated_by
+ */
+class PackageDay extends \yii\db\ActiveRecord implements \common\interfaces\NewStatusInterface
+{
+    use \common\traits\CommanRelationship;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'package_day_approval';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => \yii\behaviors\BlameableBehavior::className(),
+                'createdByAttribute' => 'created_by',
+                'updatedByAttribute' => 'updated_by',
+            ],
+            [
+                'class' => \yii\behaviors\TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => function () {
+                    return time();
+                },
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['package_id', 'day'], 'required'],
+            [['package_id', 'day', 'meal_lunch', 'meal_breakfast', 'meal_dinner', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['day_description', 'day_activity', 'day_accommodation', 'day_note'], 'string'],
+            [['day_title'], 'string', 'max' => 512],
+            [['start_location', 'end_location', 'hotel_name', 'day_image'], 'string', 'max' => 255],
+            [['package_id', 'day'], 'unique', 'targetAttribute' => ['package_id', 'day']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'package_id' => 'Package ID',
+            'day' => 'Day',
+            'day_title' => 'Day Title',
+            'day_description' => 'Day Description',
+            'start_location' => 'Start Location',
+            'end_location' => 'End Location',
+            'hotel_name' => 'Hotel Name',
+            'day_image' => 'Day Image',
+            'meal_lunch' => 'Meal Lunch',
+            'meal_breakfast' => 'Meal Breakfast',
+            'meal_dinner' => 'Meal Dinner',
+            'day_activity' => 'Day Activity',
+            'day_accommodation' => 'Day Accommodation',
+            'day_note' => 'Day Note',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    public function getImagepath()
+    {
+        if ($this->day_image != '') {
+            return '/storage/package/day/' . $this->id . '/' . $this->day_image;
+        }
+    }
+}
---
+++ common/models/packageapproval/PackageGallery.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace common\models\packageapproval;
+
+use Yii;
+
+/**
+ * This is the model class for table "package_gallery".
+ *
+ * @property int $id
+ * @property int $package_id
+ * @property string|null $image
+ * @property string|null $image_caption
+ * @property int $sequence
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $created_by
+ * @property int $updated_by
+ */
+class PackageGallery extends \yii\db\ActiveRecord implements \common\interfaces\NewStatusInterface
+{
+    use \common\traits\CommanRelationship;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'package_gallery_approval';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => \yii\behaviors\BlameableBehavior::className(),
+                'createdByAttribute' => 'created_by',
+                'updatedByAttribute' => 'updated_by',
+            ],
+            [
+                'class' => \yii\behaviors\TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => function () {
+                    return time();
+                },
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['package_id', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'required'],
+            [['package_id', 'sequence', 'status', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+            [['image'], 'string', 'max' => 255],
+            [['image_caption'], 'string', 'max' => 512],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'package_id' => 'Package ID',
+            'image' => 'Image',
+            'image_caption' => 'Image Caption',
+            'sequence' => 'Sequence',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    public function getImagepath()
+    {
+        if ($this->image != '') {
+            return '/storage/package_gallery/' . $this->id . '/' . $this->image;
+        }
+    }
+}
