c28c8b845b2b7bab9ee9510c593a276dd6840225
Akash Kumar <akashprajapatiak05@gmail.com>
2025-04-22 16:52:28 +0530

    Business Controller

---
+++ business/controllers/BusinessController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace business\controllers;
+
+use common\models\operator\SafariOperator;
+use Yii;
+use yii\web\Controller;
+
+/**
+ * BusinessRequestController.
+ */
+class BusinessController extends Controller
+{
+    public function init()
+    {
+        if (!SafariOperator::find()->where(['user_id' => \Yii::$app->user->id])->limit(1)->exists()) {
+            return $this->redirect(['/operator-registration/create']);
+        }
+    }
+}
