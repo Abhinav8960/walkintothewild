2f2ef4499161c6f8c0f13352d428a63fd91d27c3
Abhinav <abhinavpal8960@gmail.com>
2025-05-02 18:03:39 +0530

    partner reject reason i button

--- business/views/partner-registration/bankdetails.php
+++ business/views/partner-registration/bankdetails.php
@@ -18,15 +18,16 @@ use yii\bootstrap5\Html;
             <div class="accordion-item mb-3">
                 <?php if ($partner_model->form1_status == PartnerRegistration::FORM_REJECTED) { ?>
                     <h2 class="accordion-header d-flex align-items-stretch justify-content-between" id="heading1">
-                        <button id="form_1_rejectedTooltip" class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
+                        <button class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
                             data-bs-toggle="collapse" data-bs-target="#collapse1"
-                            aria-expanded="false" aria-controls="collapse1" data-bs-toggle="tooltip"
-                            data-bs-placement="right"
-                            title="<?= $partner_model->form1_reject_reason ?>">
+                            aria-expanded="false" aria-controls="collapse1">
                             Legal Entity
                             <a href="<?= Url::toRoute(['create']) ?>" class="text-decoration-none px-3">
                             <i class="bi bi-pencil text-white fs-5"></i>
-                        </a>
+                            </a>
+                            <span id="form_1_rejectedTooltip" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $partner_model->form1_reject_reason ?>">
+                            <i class="bi bi-info text-white fs-5"></i>
+                        </span>
                         </button>
                      
                     </h2>
@@ -86,15 +87,16 @@ use yii\bootstrap5\Html;
             <div class="accordion-item mb-3">
                 <?php if ($partner_model->form2_status == PartnerRegistration::FORM_REJECTED) { ?>
                     <h2 class="accordion-header d-flex align-items-stretch justify-content-between" id="heading2">
-                        <button id="form_2_rejectedTooltip" class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
+                        <button class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
                             data-bs-toggle="collapse" data-bs-target="#collapse2"
-                            aria-expanded="false" aria-controls="collapse2" data-bs-toggle="tooltip"
-                            data-bs-placement="right"
-                            title="<?= $partner_model->form2_reject_reason ?>">
+                            aria-expanded="false" aria-controls="collapse2">
                             Registration Proof
                             <a href="<?= Url::toRoute(['step-2']) ?>" class="text-decoration-none px-3">
                             <i class="bi bi-pencil text-white fs-5"></i>
                         </a>
+                        <span id="form_2_rejectedTooltip" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $partner_model->form2_reject_reason ?>">
+                            <i class="bi bi-info text-white fs-5"></i>
+                        </span>
                         </button>
                         
                     </h2>
@@ -160,15 +162,16 @@ use yii\bootstrap5\Html;
             <div class="accordion-item mb-3">
                 <?php if ($partner_model->form3_status == PartnerRegistration::FORM_REJECTED) { ?>
                     <h2 class="accordion-header d-flex align-items-stretch justify-content-between" id="heading3">
-                        <button id="form_3_rejectedTooltip" class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
+                        <button class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
                             data-bs-toggle="collapse" data-bs-target="#collapse3"
-                            aria-expanded="false" aria-controls="collapse3" data-bs-toggle="tooltip"
-                            data-bs-placement="right"
-                            title="<?= $partner_model->form3_reject_reason ?>">
+                            aria-expanded="false" aria-controls="collapse3">
                             Business Details
                             <a href="<?= Url::toRoute(['step-3']) ?>" class="text-decoration-none px-3">
                             <i class="bi bi-pencil text-white fs-5"></i>
-                        </a>
+                            </a>
+                            <span id="form_3_rejectedTooltip" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $partner_model->form3_reject_reason ?>">
+                            <i class="bi bi-info text-white fs-5"></i>
+                            </span>
                         </button>
                       
                     </h2>
@@ -228,12 +231,13 @@ use yii\bootstrap5\Html;
             <div class="accordion-item mb-3">
                 <?php if ($partner_model->form4_status == PartnerRegistration::FORM_REJECTED) { ?>
                     <h2 class="accordion-header d-flex align-items-stretch justify-content-between" id="heading4">
-                        <button id="form_4_rejectedTooltip" class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
+                        <button  class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
                             data-bs-toggle="collapse" data-bs-target="#collapse4"
-                            aria-expanded="false" aria-controls="collapse2" data-bs-toggle="tooltip"
-                            data-bs-placement="right"
-                            title="<?= $partner_model->form4_reject_reason ?>">
+                            aria-expanded="false" aria-controls="collapse2">
                             Bank Details
+                            <span id="form_4_rejectedTooltip" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $partner_model->form4_reject_reason ?>">
+                            <i class="bi bi-info text-white fs-5"></i>
+                        </span>
                         </button>
                     </h2>
 
@@ -263,15 +267,16 @@ use yii\bootstrap5\Html;
             <div class="accordion-item mb-3">
                 <?php if ($partner_model->form5_status == PartnerRegistration::FORM_REJECTED) { ?>
                     <h2 class="accordion-header d-flex align-items-stretch justify-content-between" id="heading5">
-                        <button id="form_5_rejectedTooltip" class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
+                        <button class="accordion-button collapsed flex-grow-1 text-start  background-danger text-white" type="button"
                             data-bs-toggle="collapse" data-bs-target="#collapse5"
-                            aria-expanded="false" aria-controls="collapse5" data-bs-toggle="tooltip"
-                            data-bs-placement="right"
-                            title="<?= $partner_model->form5_reject_reason ?>">
+                            aria-expanded="false" aria-controls="collapse5">
                             User KYC
                             <a href="<?= Url::toRoute(['step-5']) ?>" class="text-decoration-none px-3">
                             <i class="bi bi-pencil text-white fs-5"></i>
                         </a>
+                        <span id="form_5_rejectedTooltip" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $partner_model->form5_reject_reason ?>">
+                            <i class="bi bi-info text-white fs-5"></i>
+                        </span>
                         </button>
                       
                     </h2>
