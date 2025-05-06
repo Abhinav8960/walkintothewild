8bf39efdfbf03d558bcc81bd7c3a3184353a6c83
Abhinav <abhinavpal8960@gmail.com>
2025-05-06 19:06:30 +0530

    change in operatorapproval

--- backend/modules/operatorapproval/controllers/DefaultController.php
+++ backend/modules/operatorapproval/controllers/DefaultController.php
@@ -261,8 +261,8 @@ class DefaultController extends Controller
         $safari_operator_model->facebook_url = null;
         $safari_operator_model->instagram_url = null;
         $safari_operator_model->youtube_link = null;
-        $safari_operator_model->phone_no = $model->legal_entity_phone;
-        $safari_operator_model->email = $model->legal_entity_email;
+        $safari_operator_model->phone_no = null;
+        $safari_operator_model->email = $model->user->email;
         $safari_operator_model->website = null;
         $safari_operator_model->is_register_company = 0;
         $safari_operator_model->has_a_website = 0;
