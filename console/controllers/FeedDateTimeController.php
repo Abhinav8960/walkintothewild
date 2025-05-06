eab6ed0534877cc6fd779a1b26bb97b86f929235
“Akash <akashprajapatiak05@gmail.com>
2025-05-06 18:03:38 +0530

    Console cmd for start date

---
+++ console/controllers/FeedDateTimeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace console\controllers;
+
+use common\models\feeds\Feeds;
+use common\models\package\Package;
+use common\models\sharesafari\ShareSafari;
+use yii\console\Controller;
+
+
+
+/**
+ * FeedDateTimeController
+ */
+class FeedDateTimeController extends Controller
+{
+    public function actionShareSafari()
+    {
+        $feed_model = Feeds::find()->where(['collection' => 1])->all();
+        foreach ($feed_model as $feed) {
+            $share_safari_model = ShareSafari::find()->where(['id' => $feed->collection_id])->limit(1)->one();
+            if ($share_safari_model) {
+                $feed->date_time = date('Y-m-d H:i:s', strtotime($share_safari_model->start_date));
+                $feed->save(false);
+            }
+        }
+
+        echo "Successfully start date save!!!";
+    }
+
+    public function actionPackage()
+    {
+        $feed_model = Feeds::find()->where(['collection' => 2])->all();
+        foreach ($feed_model as $feed) {
+            $package_model = Package::find()->where(['id' => $feed->collection_id])->limit(1)->one();
+            if ($package_model) {
+                $feed->date_time = date('Y-m-d H:i:s', strtotime($package_model->start_date));
+                $feed->save(false);
+            }
+        }
+
+        echo "Successfully start date save!!!";
+    }
+}
