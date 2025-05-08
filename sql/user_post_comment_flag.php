0bcbfa47c7c4f5a7036a4a12cd4dca2105ef7ad7
“Akash <akashprajapatiak05@gmail.com>
2025-05-08 19:01:51 +0530

    Sql

---
+++ sql/user_post_comment_flag.php
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 08, 2025 at 05:08 PM
+-- Server version: 8.0.41-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `witw_staging_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_post_comment_flag`
+--
+
+DROP TABLE IF EXISTS `user_post_comment_flag`;
+CREATE TABLE `user_post_comment_flag` (
+  `id` int NOT NULL,
+  `user_posts_id` int DEFAULT NULL,
+  `user_post_comment_id` int DEFAULT NULL,
+  `flag_reason_id` int DEFAULT NULL,
+  `flag_detail` varchar(512) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  `created_at` int DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_at` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_post_comment_flag`
+--
+ALTER TABLE `user_post_comment_flag`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_post_comment_flag`
+--
+ALTER TABLE `user_post_comment_flag`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
