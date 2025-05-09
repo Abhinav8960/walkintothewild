aacff57a02f8cbbdf8bd12bc709d15dce6cb2755
Anurag <mranurag101@gmail.com>
2025-05-09 14:40:30 +0530

    changes

--- console/controllers/SendMailController.php
+++ console/controllers/SendMailController.php
@@ -57,6 +57,11 @@ class SendMailController extends Controller
                     $template = MasterMailTemplate::find()->where(['id' => $log->mail_template_id, 'status' => 1])->limit(1)->one();
                     if ($template) {
                         $mailer =  \Yii::$app->mailer;
+                        if (!isset(\Yii::$app->params['environment']) || \Yii::$app->params['environment'] != 'production') {
+                            $log->torecipient->recipient = \Yii::$app->params['adminEmail'];
+                            $bcc = [];
+                            $cc = [];
+                        }
                         $message = $mailer->compose($template->path, json_decode($log->params, true))
                             // ->setFrom($log->mail_from)
                             ->setFrom(['no-reply@walkintothewild.in' => 'Walk Into The Wild'])
