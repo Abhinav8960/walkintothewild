58acddb64009d8298ec9b55e7c16d9979e18a04e
“Akash <akashprajapatiak05@gmail.com>
2025-05-13 19:18:38 +0530

    Short Description in Park

--- api/models/park/SafariPark.php
+++ api/models/park/SafariPark.php
@@ -36,6 +36,7 @@ class SafariPark extends \common\models\park\SafariPark
             'slug',
             'feature_image_path',
             'animal_text',
+            'short_description',
             'avg_safari_price_min' => function () {
                 return (int)$this->avg_safari_price_min;
             },
