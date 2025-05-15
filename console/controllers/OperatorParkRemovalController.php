abe3932f75dd98138d2345d1082e7ffcdb0edd9e
“Akash <akashprajapatiak05@gmail.com>
2025-05-15 16:57:54 +0530

    Operator Park Removal

---
+++ console/controllers/OperatorParkRemovalController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace console\controllers;
+
+use common\models\operator\SafariOperator;
+use common\models\operator\SafariOperatorPark;
+use common\models\sharesafari\ShareSafari;
+use common\models\User;
+use Yii;
+use yii\console\Controller;
+
+
+
+/**
+ * OperatorRemovalController
+ */
+class OperatorParkRemovalController extends Controller
+{
+    public function actionRemove()
+    {
+        $safari_operator_ids = [76, 23, 4, 3, 95, 94];
+
+        $excludedIds = SafariOperatorPark::find()
+            ->where(['safari_operator_id' => $safari_operator_ids, 'status' => SafariOperatorPark::STATUS_ACTIVE])
+            ->all();
+
+
+        $safari_operator_parks = SafariOperatorPark::find()
+            ->where(['not in', 'safari_operator_id', $excludedIds])
+            ->all();
+
+        foreach ($safari_operator_parks as $safari_operator_park) {
+            $safari_operator_park->status = 0;
+            $safari_operator_park->save(false);
+        }
+
+    }
+}
