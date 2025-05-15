d279af40136032051377c815b0c14a21acc80456
Anurag <mranurag101@gmail.com>
2025-05-15 14:35:46 +0530

    changes

--- common/events/sharesafari/SafariUnjoinedByuser.php
+++ common/events/sharesafari/SafariUnjoinedByuser.php
@@ -2,7 +2,7 @@
 
 namespace common\events\sharesafari;
 
-use api\models\sharesafari\ShareSafari;
+use common\models\sharesafari\ShareSafari;
 use common\broadcast\services\BroadcastService;
 use common\models\master\email\MasterMailTemplate;
 use common\models\master\notification\MasterNotificationTemplate;
@@ -139,12 +139,20 @@ class SafariUnjoinedByuser extends Event
 
     public function prepareData()
     {
+        
         $this->shared_safari = ShareSafari::find()->where(['id' => $this->shared_safari_id])->one();
-        $this->shared_safari_name = $this->shared_safari['share_safari_title'];
-        $this->shared_safari_url = urlencode(\Yii::$app->frontendUrlManager->createAbsoluteUrl(['/sharedsafari/default/view', 'slug' => $this->shared_safari['slug'], 'organized_slug' => $this->shared_safari['organizedslug'] ? $this->shared_safari['organizedslug'] : '']));
+        $this->shared_safari_name = $this->shared_safari->share_safari_title;
+        $this->shared_safari_url = urlencode(\Yii::$app->frontendUrlManager->createAbsoluteUrl(['/sharedsafari/default/view', 'slug' => $this->shared_safari->slug, 'organized_slug' => $this->shared_safari->organizedslug ? $this->shared_safari->organizedslug : '']));
         $this->userId = $this->shared_safari->host_user_id;
-        $this->email = $this->shared_safari->user->email;
-        $this->name =  $this->shared_safari->user->name;
+        if ($this->shared_safari->type == ShareSafari::TYPE_FIXED_DEPARTURE) {
+
+            $this->email = $this->shared_safari->safarioperator->email;
+            $this->name =  $this->shared_safari->safarioperator->business_name;
+        } else {
+            $this->email = $this->shared_safari->user->email;
+            $this->name =  $this->shared_safari->user->name;
+        }
+        
         // $this->shared_safari_url = urlencode("http://walkintothewild.io". $this->shared_safari['slug']);
         $this->sent_data = [
             'share_safari_title' => $this->shared_safari['share_safari_title'],
