aa638a10aa42621f880fa95ac2c141c593b6703f
“Akash <akashprajapatiak05@gmail.com>
2025-05-16 12:54:51 +0530

    History API for share Safari

---
+++ api/models/sharesafari/ShareSafariHistory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace api\models\sharesafari;
+
+use api\models\park\SafariPark;
+use api\models\User;
+use Yii;
+
+class ShareSafariHistory extends \common\models\sharesafari\ShareSafariHistory
+{
+   
+
+    public function getPark()
+    {
+        return $this->hasOne(SafariPark::className(), ['id' => 'park_id']);
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'host_user_id']);
+    }
+}
