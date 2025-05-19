30688fcf3a0d986438b278f90934158d7b607966
“Akash <akashprajapatiak05@gmail.com>
2025-05-19 17:18:37 +0530

    sql

---
+++ sql/user_delete_request.php
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 19, 2025 at 05:16 PM
+-- Server version: 8.0.42-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `witw_12`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_delete_request`
+--
+
+DROP TABLE IF EXISTS `user_delete_request`;
+CREATE TABLE `user_delete_request` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `email` varchar(512) NOT NULL,
+  `created_at` int DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_at` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_delete_request`
+--
+ALTER TABLE `user_delete_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_delete_request`
+--
+ALTER TABLE `user_delete_request`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
