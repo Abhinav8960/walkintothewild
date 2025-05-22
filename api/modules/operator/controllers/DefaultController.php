0916975ae96a1d8241bdf83be79ca0b32a3c125b
“Akash <akashprajapatiak05@gmail.com>
2025-05-22 13:21:35 +0530

    Package Status

--- api/modules/operator/controllers/DefaultController.php
+++ api/modules/operator/controllers/DefaultController.php
@@ -371,6 +371,7 @@ class DefaultController extends RestController
 
         $searchModel = new PackageSearch();
         $searchModel->owned_by_id = $operator->id;
+        $searchModel->status = Package::STATUS_ACTIVE;
         $condition = ['not', ['live_version' => null]];
         return $this->dataProviderSenderWithCondition($searchModel, "packages", $condition);
         // return Yii::$app->api->sendResponse($data = ['operatorpackage' => $this->serializeData($operator->packages)]);
