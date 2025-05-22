8c63f3fd670b6a21b064adb3bfaede4abbdc8f52
Anurag <mranurag101@gmail.com>
2025-05-22 08:20:01 +0530

    email remove unwanted things

--- common/mail/userRegistration-html.php
+++ common/mail/userRegistration-html.php
@@ -11,12 +11,4 @@ use yii\helpers\Html;
     <p>Welcome to Walk Into The Wild!</p>
     <p>We are thrilled to have you on board and to see you join our community. Your registration was successful, and you are now part of an exciting journey with us.</p>
 </div>
-    <div class="card">
-        <div class="card-body">
-            <p>Sample Array :</p>
-            <p>1. username</p>
-
-            <p>Expected Key :</p>
-            <p>1. Annu Singh</p>
-        </div>
-    </div>
+   
