b71ceb4efa376302bb200f03143e75c5148a7872
“Akash <akashprajapatiak05@gmail.com>
2025-05-29 14:46:18 +0530

    API : Partner Gallery View

---
+++ api/models/partnergallery/PartnerGallerySearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace api\models\partnergallery;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * PartnerGallerySearch represents the model behind the search form of PartnerGallery.
+ */
+class PartnerGallerySearch extends PartnerGallery
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['safari_operator_id', 'title', 'safari_park_id'], 'safe'],
+            [['safari_operator_id', 'safari_park_id', 'status', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = PartnerGallery::find()->where(['status' => [PartnerGallery::STATUS_ACTIVE, PartnerGallery::STATUS_SUSPEND]]);
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'safari_operator_id' => $this->safari_operator_id,
+            'created_at' => $this->created_at,
+            'created_by' => $this->created_by,
+            'updated_at' => $this->updated_at,
+            'updated_by' => $this->updated_by,
+            'status' => $this->status,
+        ]);
+
+        return $dataProvider;
+    }
+}
---
+++ api/models/partnergalleryimage/PartnerGalleryImage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace api\models\partnergalleryimage;
+
+use Yii;
+
+
+class PartnerGalleryImage extends \common\models\partnergalleryimage\PartnerGalleryImage
+{
+
+    public function fields()
+    {
+
+
+        $fields = [
+            'original_filename',
+            'gallery_image_path',
+            'file',
+            'title',
+            'caption',
+            'sequence',
+            'status'
+        ];
+
+        return $fields;
+    }
+    public function getGallery_image_path()
+    {
+        if ($this->filepath) {
+            return Yii::$app->params['s3_endpoint'] . '/' . $this->filepath;
+        }
+        return '';
+    }
+}
