dcb3e029c28670ce4774cab7c5f2ba3bab5708fc
“Akash <akashprajapatiak05@gmail.com>
2025-05-29 14:38:44 +0530

    Force copy

--- business/assets/NovaAppAsset.php
+++ business/assets/NovaAppAsset.php
@@ -21,7 +21,7 @@ class NovaAppAsset extends AssetBundle
         // 'plugins/select2/css/select2.min.css',
     ];
     public $publishOptions = [
-        'forceCopy' => true,
+        'forceCopy' => false,
     ];
     public $js = [
         'js/script.js',
