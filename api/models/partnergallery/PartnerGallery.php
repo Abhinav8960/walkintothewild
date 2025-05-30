4d72e333f54e181b0e400dc3d062d4d83e62b457
“Akash <akashprajapatiak05@gmail.com>
2025-05-30 13:35:58 +0530

    Change

--- api/models/partnergallery/PartnerGallery.php
+++ api/models/partnergallery/PartnerGallery.php
@@ -3,6 +3,7 @@
 namespace api\models\partnergallery;
 
 use api\models\park\SafariPark;
+use api\models\partnergalleryimage\PartnerGalleryImage;
 use Yii;
 
 class PartnerGallery extends \common\models\partnergallery\PartnerGallery
@@ -18,6 +19,7 @@ class PartnerGallery extends \common\models\partnergallery\PartnerGallery
             'safari_park_label',
             'slug',
             'url',
+            'thumbnail',
             'status'
         ];
 
@@ -32,7 +34,15 @@ class PartnerGallery extends \common\models\partnergallery\PartnerGallery
     public function getUrl()
     {
         return Yii::$app->params['api_url'] . '/manage/gallery/' . $this->slug . '/gallery-images';
-        
+    }
+
+    public function getThumbnail()
+    {
+        $model = PartnerGalleryImage::find()->where(['partner_gallery_id' => $this->id])->andWhere(['set_as_thumbnail' => 1])->limit(1)->one();
+        if ($model) {
+            return Yii::$app->params['s3_endpoint'] . '/' . $model->filepath;
+        }
+        return null;
     }
 
     public function getSafari_park_label()
