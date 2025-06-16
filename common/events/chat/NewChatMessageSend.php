bc23e6c9b53777f78a2c1b72aae31bdafab32918
Anurag <mranurag101@gmail.com>
2025-06-16 17:21:42 +0530

    working on notification deep-linking

--- common/events/chat/NewChatMessageSend.php
+++ common/events/chat/NewChatMessageSend.php
@@ -32,7 +32,7 @@ class NewChatMessageSend extends Event
         $this->sender_user_handle   = $sender_user_handle;
         $this->message              = $message;
         $this->objective            =   Chat::OBJECTIVE_QUOTE;
-        if ($this->chat->chat_type == Chat::CHAT_TYPE_DIRECT) {
+        if ($chat->chat_type == Chat::CHAT_TYPE_DIRECT) {
             $this->objective            =  Chat::OBJECTIVE_DIRECT;
         }
         $this->chat_hash            = $chat_hash;
