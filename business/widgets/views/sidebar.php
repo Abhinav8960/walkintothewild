453522711db869035dfb7f223da44af0ab1a9bdc
Akash <akashprajapatiak05@gmail.com>
2025-06-16 11:29:29 +0530

    Hide unecessary details

--- business/widgets/views/sidebar.php
+++ business/widgets/views/sidebar.php
@@ -14,66 +14,67 @@ $active_url = "/" . Yii::$app->requestedRoute;
             </div>
         </li>
         <li class="navItems mb-2">
-            <a href="/" class="navLinks d-flex align-items-center <?= in_array($active_url,["/"]) ? "active" : "" ?>">
+            <a href="/" class="navLinks d-flex align-items-center <?= in_array($active_url, ["/"]) ? "active" : "" ?>">
                 <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
                 <span class="hide-slide-menu"> Dashboard</span>
             </a>
         </li>
         <li class="navItems mb-2">
             <a class="navLinks  d-flex align-items-center <?= in_array($active_url, array(
-                                                    "/leads/default/index",
-                                                    "/leads/default/create",
-                                                     "/leads/default/view",
-                                                )) ? "active" : "" ?>" href="/leads/default/index">
+                                                                "/leads/default/index",
+                                                                "/leads/default/create",
+                                                                "/leads/default/view",
+                                                            )) ? "active" : "" ?>" href="/leads/default/index">
                 <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
                 <span class="hide-slide-menu"> Leads</span>
             </a>
         </li>
-        <li class="navItems mb-2">
-            <a class="navLinks d-flex align-items-center <?= in_array($active_url, array(
-                                                    "/package/default/index",
-                                                    "/package/default/create",
-                                                )) ? "active" : "" ?>" href="/package/default/index">
-                <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
-                <span class="hide-slide-menu"> My Packages</span>
-            </a>
-        </li>
-        <!-- <li class="navItems mb-2">
+        <?php if (false) { ?>
+            <li class="navItems mb-2">
+                <a class="navLinks d-flex align-items-center <?= in_array($active_url, array(
+                                                                    "/package/default/index",
+                                                                    "/package/default/create",
+                                                                )) ? "active" : "" ?>" href="/package/default/index">
+                    <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
+                    <span class="hide-slide-menu"> My Packages</span>
+                </a>
+            </li>
+            <!-- <li class="navItems mb-2">
             <a href="" class="navLinks  d-flex align-items-center">
                 <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
                 <span class="hide-slide-menu"> My Fixed Departures</span>
             </a>
         </li> -->
-        <li class="navItems mb-2">
-            <a class="navLinks d-flex align-items-center <?= in_array($active_url, array(
-                                                    "/sightings/default/index",
-                                                    "/sightings/default/create",
-                                                )) ? "active" : "" ?>" href="/sightings/default/index">
-                <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
-                <span class="hide-slide-menu"> Sightings</span>
-            </a>
-        </li>
-        <li class="navItems mb-2">
-            <a class="navLinks  d-flex align-items-center <?= in_array($active_url, array(
-                                                                "/posts/default/index",
-                                                                "/posts/default/create",
+            <li class="navItems mb-2">
+                <a class="navLinks d-flex align-items-center <?= in_array($active_url, array(
+                                                                    "/sightings/default/index",
+                                                                    "/sightings/default/create",
+                                                                )) ? "active" : "" ?>" href="/sightings/default/index">
+                    <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
+                    <span class="hide-slide-menu"> Sightings</span>
+                </a>
+            </li>
+            <li class="navItems mb-2">
+                <a class="navLinks  d-flex align-items-center <?= in_array($active_url, array(
+                                                                    "/posts/default/index",
+                                                                    "/posts/default/create",
 
-                                                            )) ? "active" : "" ?>" href="/posts/default/index">
-                <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
-                <span class="hide-slide-menu"> Posts</span>
-            </a>
-        </li>
-        <li class="navItems mb-2">
-            <a class="navLinks  d-flex align-items-center <?= in_array($active_url, array(
-                                                                "/gallery/default/index",
-                                                                "/gallery/default/create",
+                                                                )) ? "active" : "" ?>" href="/posts/default/index">
+                    <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
+                    <span class="hide-slide-menu"> Posts</span>
+                </a>
+            </li>
+            <li class="navItems mb-2">
+                <a class="navLinks  d-flex align-items-center <?= in_array($active_url, array(
+                                                                    "/gallery/default/index",
+                                                                    "/gallery/default/create",
 
-                                                            )) ? "active" : "" ?>" href="/gallery/default/index">
-                <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
-                <span class="hide-slide-menu"> Gallery</span>
-            </a>
-        </li>
-        <!-- <li class="navItems mb-2">
+                                                                )) ? "active" : "" ?>" href="/gallery/default/index">
+                    <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
+                    <span class="hide-slide-menu"> Gallery</span>
+                </a>
+            </li>
+            <!-- <li class="navItems mb-2">
             <a href="" class="navLinks d-flex align-items-center">
                 <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
                 <span class="hide-slide-menu"> Reports</span>
@@ -85,7 +86,7 @@ $active_url = "/" . Yii::$app->requestedRoute;
                 <span class="hide-slide-menu"> Settings</span>
             </a>
         </li> -->
-
+        <?php } ?>
         <li class="navItems mb-2">
             <a class="navLinks  d-flex align-items-center" href="<?= \yii\helpers\Url::to('/site/logout') ?>" data-method="post">
                 <span class="nav-icon me-1"> <i class="fa-solid fa-house-chimney"></i></span>
