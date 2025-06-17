ce8d375c4fc74bb160cb9d6ec6f24ea9e4200978
Akash <akashprajapatiak05@gmail.com>
2025-06-17 16:19:26 +0530

    Custom Pager

--- business/modules/leads/views/default/index.php
+++ business/modules/leads/views/default/index.php
@@ -14,101 +14,104 @@ $this->title = 'Leads';
 <?php echo $this->render('_search', ['model' => $searchModel, 'safari_operator' => $safari_operator]); ?>
 <div class="table-wrapper">
     <div class="table-responsive">
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'layout' => "{items}\n<div class='row align-items-center mt-3'>
-                            <div class='col-md-4 text-start'>{summary}</div>
-                            <div class='col-md-4 text-center'>{pager}</div>
+        <div class="min-width-table">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'layout' => "{items}\n<div class='row align-items-center mt-3'>
+                            <div class='col-md-4 text-start mb-2'>{summary}</div>
+                            <div class='col-md-4 text-center mb-2'>{pager}</div>
+                            <div class='col-md-4'></div>
                         </div>",
-            'tableOptions' => ['class' => 'table tablecustoms table-striped align-middle w-100'],
-            'columns' => [
-                [
-                    'class' => 'yii\grid\SerialColumn',
-                    'headerOptions' => ['style' => 'width: 5%;'],
-                ],
-                [
-                    'label' => 'Source',
-                    'headerOptions' => ['style' => 'width: 15%;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return $model->sourceLabelWithBadge;
-                    }
-                ],
-
-                [
-                    'label' => 'Display Label',
-                    'headerOptions' => ['style' => 'width: 15%;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return $model->displayLabel;
-                    }
-                ],
-
-                [
-                    'label' => 'Safaris',
-                    'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return !empty($model->safaris) ? $model->safaris : '';
-                    }
-                ],
-                [
-                    'label' => 'Travelers',
-                    'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return !empty($model->travelers) ? $model->travelers : '';
-                    }
-                ],
-
-                [
-                    'label' => 'Accomodation',
-                    'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return !empty($model->staycatgory) ? $model->staycatgory->title : '';
-                    }
-                ],
-                [
-                    'label' => 'Travel Date',
-                    'headerOptions' => ['style' => 'width: 15%;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        $str =  date('d M, Y', strtotime($model->from_date));
-                        if (!empty($model->to_date)) {
-                            $str .=  '- ' . date('d M, Y', strtotime($model->to_date));
+                'tableOptions' => ['class' => 'table tablecustoms table-striped align-middle w-100'],
+                'columns' => [
+                    [
+                        'class' => 'yii\grid\SerialColumn',
+                        'headerOptions' => ['style' => 'width: 5%;'],
+                    ],
+                    [
+                        'label' => 'Source',
+                        'headerOptions' => ['style' => 'width: 15%;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return $model->sourceLabelWithBadge;
                         }
-                        return $str;
-                    }
+                    ],
+
+                    [
+                        'label' => 'Display Label',
+                        'headerOptions' => ['style' => 'width: 15%;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return $model->displayLabel;
+                        }
+                    ],
+
+                    [
+                        'label' => 'Safaris',
+                        'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return !empty($model->safaris) ? $model->safaris : '';
+                        }
+                    ],
+                    [
+                        'label' => 'Travelers',
+                        'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return !empty($model->travelers) ? $model->travelers : '';
+                        }
+                    ],
+
+                    [
+                        'label' => 'Accomodation',
+                        'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return !empty($model->staycatgory) ? $model->staycatgory->title : '';
+                        }
+                    ],
+                    [
+                        'label' => 'Travel Date',
+                        'headerOptions' => ['style' => 'width: 15%;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            $str =  date('d M, Y', strtotime($model->from_date));
+                            if (!empty($model->to_date)) {
+                                $str .=  '- ' . date('d M, Y', strtotime($model->to_date));
+                            }
+                            return $str;
+                        }
+                    ],
+
+                    [
+                        'label' => 'Lead Received',
+                        'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return date('d M, Y h:i A', $model->created_at);
+                        }
+                    ],
+
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'header' => "Actions",
+                        'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
+                        'template' => '{view}',
+                        'buttons' => [
+
+                            'view' => function ($url, $model) {
+                                return  Html::a('<i class="mdi mdi-eye"></i>', ['/leads/default/view', 'id' => $model->id], [
+                                    'class' => 'action-icon',
+                                    'title' => 'View',
+                                ]);
+                            },
+
+
+                        ]
+                    ],
                 ],
-
-                [
-                    'label' => 'Lead Received',
-                    'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
-                    'format' => 'raw',
-                    'value' => function ($model) {
-                        return date('d M, Y h:i A', $model->created_at);
-                    }
-                ],
-
-                [
-                    'class' => 'yii\grid\ActionColumn',
-                    'header' => "Actions",
-                    'contentOptions' => ['style' => 'width: 10%; text-align: left;'],
-                    'template' => '{view}',
-                    'buttons' => [
-
-                        'view' => function ($url, $model) {
-                            return  Html::a('<i class="mdi mdi-eye"></i>', ['/leads/default/view', 'id' => $model->id], [
-                                'class' => 'action-icon',
-                                'title' => 'View',
-                            ]);
-                        },
-
-
-                    ]
-                ],
-            ],
-        ]); ?>
+            ]); ?>
+        </div>
     </div>
 </div>
