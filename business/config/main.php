c3758c547e27ebbd1756d37f3d51cb74b52c19ad
Akash <akashprajapatiak05@gmail.com>
2025-06-17 14:50:02 +0530

    Pagination

--- business/config/main.php
+++ business/config/main.php
@@ -98,9 +98,16 @@ return [
         'definitions' => [
             \yii\widgets\LinkPager::class => \yii\bootstrap5\LinkPager::class,
             'yii\bootstrap5\LinkPager' => [
-                'firstPageLabel' => 'First',
-                'lastPageLabel' => 'Last',
-                'options' => ['class' => 'pagination pagination-primary mg-sm-b-0']
+                'options' => ['class' => 'pagination mb-0 d-flex gap-4 align-items-center'],
+                'listOptions' => ['class' => ['pagination mb-0 d-flex gap-4 align-items-center']],
+                'linkOptions' => ['class' => ''],
+                'linkContainerOptions' => ['class' => ['page-item']],
+                'disabledPageCssClass' => 'disabled',
+                'activePageCssClass' => 'active',
+                'prevPageLabel' => '<span class="page-link button_nextprve">Prev</span>',
+                'nextPageLabel' => '<span class="page-link button_nextprve active">Next</span>',
+                'firstPageLabel' => false,
+                'lastPageLabel' => false,
             ],
         ],
     ],
