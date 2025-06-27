fd7c58353984b07cad22a939efe4171ca2ccb99f
Abhinav <abhinavpal8960@gmail.com>
2025-06-27 14:21:48 +0530

    email verification template change

--- common/mail/emailotpverification-html.php
+++ common/mail/emailotpverification-html.php
@@ -2,33 +2,6 @@
 
 use yii\helpers\Html;
 
-$remainingMinutes = 0;
-$remainingSeconds = 0;
-$expiryMessage = 'expired';
-
-if (!empty($exp_datetime)) {
-    $now = new \DateTime();
-    $expiry = new \DateTime($exp_datetime);
-
-    if ($expiry > $now) {
-        $interval = $now->diff($expiry);
-        $minutes = $interval->i;
-        $seconds = $interval->s;
-
-        // Total duration message
-        if ($interval->h > 0) {
-            $expiryMessage = $interval->h . ' hour' . ($interval->h > 1 ? 's' : '');
-            if ($minutes > 0) {
-                $expiryMessage .= ' ' . $minutes . ' minute' . ($minutes > 1 ? 's' : '');
-            }
-        } elseif ($minutes > 0) {
-            $expiryMessage = $minutes . ' minute' . ($minutes > 1 ? 's' : '');
-        } else {
-            $expiryMessage = $seconds . ' second' . ($seconds > 1 ? 's' : '');
-        }
-    }
-}
-
 ?>
 
 <html>
@@ -42,10 +15,11 @@ if (!empty($exp_datetime)) {
         <div style="border-radius: 15px; margin-top:20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); transition: 0.3s; text-align: center; padding: 10px; background-color:#ecedf1;">
             <p style="font-family: Arial, sans-serif;"> 
                 <?= isset($username) ? Html::encode($username) : '' ?>, 
-                your One-Time Password is 
+                your One-Time Password is <br>
                 <strong><?= isset($email_otp) ? Html::encode($email_otp) : '' ?></strong>. 
+                <br>
                 It will expire in 
-                <strong><?= $expiryMessage ?></strong>.
+                <strong> 5 minutes </strong>.
             </p>
         </div>
         <footer style="margin-top: 80px; margin-bottom:0px;font-size: 12px; color: #888;">
