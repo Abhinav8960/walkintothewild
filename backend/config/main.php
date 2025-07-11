0da35e5e95176a93825368b5bc0f9b230f6683f0
Anurag <mranurag101@gmail.com>
2025-07-11 16:24:29 +0530

    changes

--- backend/config/main.php
+++ backend/config/main.php
@@ -126,84 +126,85 @@ return [
         ],
         'transaction' => [
             'class' => 'backend\modules\transaction\Module',
-        'chat' => [
-            'class' => 'backend\modules\chat\Module',
-        ],
-
-    ],
-    'components' => [
-        'reCaptcha3' => [
-            'class'      => 'kekaadrenalin\recaptcha3\ReCaptcha',
-            'site_key'   => isset($_SERVER['GOOGLE_CAPTCHA_SITE_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SITE_KEY'] : '6LdlvuYpAAAAAK2nW4xcNThJOMxVl2S6cGKqVJ9C',
-            'secret_key' => isset($_SERVER['GOOGLE_CAPTCHA_SECRET_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SECRET_KEY'] : '6LdlvuYpAAAAABTlzZZ2dSAH3BhHL9WkxG7gfyUi',
-        ],
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-        'user' => [
-            'class' => 'common\components\WebUser', // For Tracking the Sessions
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'class' => 'yii\web\DbSession',
-            'name' => 'advanced-backend',
-            'timeout' => 3600 * 24 * 30,
-            'cookieParams' => [
-                'lifetime' => 3600 * 24 * 30, // Cookie lifetime, e.g., 30 days
+            'chat' => [
+                'class' => 'backend\modules\chat\Module',
             ],
+
         ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => \yii\log\FileTarget::class,
-                    'levels' => ['error', 'warning'],
-                ],
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['info'],
-                    'categories' => ['payu'],
-                    'logFile' => '@runtime/logs/payu.log',
-                    'maxFileSize' => 1024 * 2, // 2MB
-                    'maxLogFiles' => 5,
+        'components' => [
+            'reCaptcha3' => [
+                'class'      => 'kekaadrenalin\recaptcha3\ReCaptcha',
+                'site_key'   => isset($_SERVER['GOOGLE_CAPTCHA_SITE_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SITE_KEY'] : '6LdlvuYpAAAAAK2nW4xcNThJOMxVl2S6cGKqVJ9C',
+                'secret_key' => isset($_SERVER['GOOGLE_CAPTCHA_SECRET_KEY']) ? $_SERVER['GOOGLE_CAPTCHA_SECRET_KEY'] : '6LdlvuYpAAAAABTlzZZ2dSAH3BhHL9WkxG7gfyUi',
+            ],
+            'request' => [
+                'csrfParam' => '_csrf-backend',
+            ],
+            'user' => [
+                'class' => 'common\components\WebUser', // For Tracking the Sessions
+                'identityClass' => 'common\models\User',
+                'enableAutoLogin' => true,
+                'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            ],
+            'session' => [
+                // this is the name of the session cookie used for login on the backend
+                'class' => 'yii\web\DbSession',
+                'name' => 'advanced-backend',
+                'timeout' => 3600 * 24 * 30,
+                'cookieParams' => [
+                    'lifetime' => 3600 * 24 * 30, // Cookie lifetime, e.g., 30 days
                 ],
             ],
-        ],
-        'errorHandler' => [
-            // 'class' => '\bedezign\yii2\audit\components\web\ErrorHandler',
-            'errorAction' => 'site/error',
-            // 'class' => 'yii\web\ErrorHandler',
-            'on ' . \yii\web\Response::EVENT_BEFORE_SEND => function ($event) {
-                // Check if the response status code is not already set
-                if ($event->sender->statusCode == null || $event->sender->statusCode == 200) {
-                    // Set the response status code to 500 (Internal Server Error)
-                    $event->sender->setStatusCode(500);
-                }
-            },
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-                '/re/<short_id>' => 'site/redirect-url',
-                '/aws-mailer-notification' => '/aws-mailer-notification/index',
-                'payu-response' => 'payment-response/payu-response',
+            'log' => [
+                'traceLevel' => YII_DEBUG ? 3 : 0,
+                'targets' => [
+                    [
+                        'class' => \yii\log\FileTarget::class,
+                        'levels' => ['error', 'warning'],
+                    ],
+                    [
+                        'class' => 'yii\log\FileTarget',
+                        'levels' => ['info'],
+                        'categories' => ['payu'],
+                        'logFile' => '@runtime/logs/payu.log',
+                        'maxFileSize' => 1024 * 2, // 2MB
+                        'maxLogFiles' => 5,
+                    ],
+                ],
             ],
-        ],
+            'errorHandler' => [
+                // 'class' => '\bedezign\yii2\audit\components\web\ErrorHandler',
+                'errorAction' => 'site/error',
+                // 'class' => 'yii\web\ErrorHandler',
+                'on ' . \yii\web\Response::EVENT_BEFORE_SEND => function ($event) {
+                    // Check if the response status code is not already set
+                    if ($event->sender->statusCode == null || $event->sender->statusCode == 200) {
+                        // Set the response status code to 500 (Internal Server Error)
+                        $event->sender->setStatusCode(500);
+                    }
+                },
+            ],
+            'urlManager' => [
+                'enablePrettyUrl' => true,
+                'showScriptName' => false,
+                'rules' => [
+                    '/re/<short_id>' => 'site/redirect-url',
+                    '/aws-mailer-notification' => '/aws-mailer-notification/index',
+                    'payu-response' => 'payment-response/payu-response',
+                ],
+            ],
 
-    ],
-    'container' => [
-        'definitions' => [
-            \yii\widgets\LinkPager::class => \yii\bootstrap5\LinkPager::class,
-            'yii\bootstrap5\LinkPager' => [
-                'firstPageLabel' => 'First',
-                'lastPageLabel' => 'Last',
-                'options' => ['class' => 'pagination pagination-primary mg-sm-b-0']
+        ],
+        'container' => [
+            'definitions' => [
+                \yii\widgets\LinkPager::class => \yii\bootstrap5\LinkPager::class,
+                'yii\bootstrap5\LinkPager' => [
+                    'firstPageLabel' => 'First',
+                    'lastPageLabel' => 'Last',
+                    'options' => ['class' => 'pagination pagination-primary mg-sm-b-0']
+                ],
             ],
         ],
-    ],
-    'params' => $params,
+        'params' => $params,
+    ]
 ];
