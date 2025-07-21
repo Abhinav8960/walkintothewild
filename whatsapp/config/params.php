fc27f56110605210674e941b9cc41b7a1f07d09f
Anurag <mranurag101@gmail.com>
2025-07-21 18:52:51 +0530

    whatsapp application remove

--- whatsapp/config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'bsVersion' => '5.x',
-    "isGoogleV3CaptchaValidateNeeded" => false,
-    'senderEmail' => 'smritipal2201@gmail.com',
-    'senderName' => 'Walk Into Wild',
-    'mailerActive' => true,
-    'app_name' => 'Backend'
-];
