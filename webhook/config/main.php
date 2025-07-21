ac05343a2a7d34e30ce8ef9faff10f1a813fd957
Anurag <mranurag101@gmail.com>
2025-07-21 18:50:26 +0530

    changfes

--- webhook/config/main.php
+++ webhook/config/main.php
@@ -12,7 +12,7 @@ return [
     'controllerNamespace' => 'webhook\controllers',
     'bootstrap' => ['log', '\webhook\components\AppBootstrap'],
     'timeZone' => 'Asia/Calcutta',
-   
+
     'components' => [
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -37,7 +37,15 @@ return [
                     'maxFileSize' => 1024 * 2, // 2MB
                     'maxLogFiles' => 5,
                 ],
-                
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['info'],
+                    'categories' => ['whatsapp-webhook'],
+                    'logFile' => '@runtime/logs/whatsapp-webhook.log',
+                    'maxFileSize' => 1024 * 2, // 2MB
+                    'maxLogFiles' => 5,
+                ],
+
             ],
         ],
         'user' => [
@@ -70,7 +78,7 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [               
+            'rules' => [
                 'payu-response' => 'payment-response/payu-response',
             ],
         ],
---
+++ webhook/controllers/WhatsappController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace webhook\controllers;
+
+
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii;
+use Netflie\WhatsAppCloudApi\WebHook;
+
+/**
+ * Default controller for the `error` module
+ */
+class WhatsappController extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                    ],
+
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'index' => ['POST', 'GET']
+                ],
+            ],
+        ];
+    }
+
+
+    public function beforeAction($action)
+    {
+        if (in_array($action->id, ['index'])) {
+            // Disable CSRF validation for the payu-response action
+            $this->enableCsrfValidation = false;
+        }
+        return parent::beforeAction($action);
+    }
+
+    public function actionIndex()
+    {
+         /* ------------TO VERIFY TOKEN (UnCOMMENT ME)---- */
+        $webhook = new WebHook();
+
+        echo $webhook->verify($_GET, "white-elephant");
+        die();
+        /* --------------------------------------------------------- */
+
+        $data = Yii::$app->request->post();        
+        \Yii::info('PayU Response Sucess: ' . json_encode($data), 'whatsapp-webhook');
+    }
+}
