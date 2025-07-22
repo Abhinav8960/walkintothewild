7b6963f117a99453b78cdcfa737dfc8c8cd4e3e0
Sarwar <mdsarwa1575@gmail.com>
2025-07-22 17:44:41 +0530

    created pages

--- support/modules/operator/views/safari-operator/_navbar.php
+++ support/modules/operator/views/safari-operator/_navbar.php
@@ -235,10 +235,221 @@ use yii\helpers\Url;
                 
             </div>
 
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">comingsoon!</div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
-            <div class="tab-pane fade" id="con" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
-            <div class="tab-pane fade" id="fivcont" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                 <div class="row">
+                    <div class="col-xl-9">
+                     <div class="overviewDataParent">
+                    <table class="table w-100 border-0 border_o d-inline-block py-3 bg-white">
+                        <tbody class="tbody-leads sighting-leads py-5 w-100">
+                            <tr>
+                                <td style="width: 60%;">Business Name:</td>
+                                <td style="width: 50%;">
+                                    <p>Shivsakti</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Address:</td>
+                                <td>
+                                    <p>Noida sector 62</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Phone Number: </td>
+                                <td>
+                                    <p>8825317553</p>
+                                </td>
+                            </tr>
+                                 <tr>
+                                <td>Legal entity Email:</td>
+                                <td>
+                                    <p>annu@gmail.com</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>Category:</td>
+                                <td>
+                                    <p>Safari Tour Operator</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>PAN Number:</td>
+                                <td>
+                                    <p>DFY1533SF</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>Approved Status:</td>
+                                <td>
+                                    <p>Yes</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                       </div>
+                    </div>
+                    <div class="col-xl-3">
+                        <div class="identification-photo">
+                            <p class="mb-1">Pan Photo: </p>
+                               <a href="/">
+                                  <img src="<?= $this->params['baseurl'] ?>/images/pancard.png" alt="" class="w-100 h-100 object-fit-cover">
+                              </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                <div class="row">
+                    <div class="col-xl-3 pb-4">
+                     
+                        <div class="card cardMainImage h-100">
+                                <div class="d-flex justify-content-between align-items-center mb-2 cardText">
+                                    <span >Registration Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                             <img
+                                    src="<?= $this->params['baseurl'] ?>/images/prof.png" class="card-img-top object-fit-cover w-100"
+                                    alt="">
+                    
+                   
+                  </div>
+                    </div>
+                      <div class="col-xl-3 pb-4">
+                     
+                        <div class="card cardMainImage h-100">
+                                <div class="d-flex justify-content-between align-items-center mb-2 cardText">
+                                    <span >PAN Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                             <img
+                                    src="<?= $this->params['baseurl'] ?>/images/prof.png" class="card-img-top object-fit-cover w-100"
+                                    alt="">
+                    
+                   
+                  </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="con" role="tabpanel" aria-labelledby="contact-tab">
+                <div class="businessMainParent">
+                     <table class="table w-100 border-0 border_o d-inline-block py-3 bg-white">
+                        <tbody class="tbody-leads sighting-leads py-5 w-100">
+                            <tr>
+                                <td style="width: 17%;">Operated Park:</td>
+                                <td style="width: 83%;">
+                                    <p>oindrila.pharma</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>About Business:</td>
+                                <td>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, </p>
+                                </td>
+                            </tr>
+                          
+                        </tbody>
+                    </table>
+                    <div class="row">
+                    <div class="col-xl-3 pb-4">
+                     
+                        <div class="card cardMainImage h-100">
+                                <div class="d-flex justify-content-between align-items-center mb-3 cardText">
+                                    <span >Registration Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                          <div class="d-flex justify-content-between align-items-center mb-2 cardText">
+                                    <span >PAN Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                             <img
+                                    src="<?= $this->params['baseurl'] ?>/images/prof.png" class="card-img-top object-fit-cover w-100"
+                                    alt="">
+                    
+                   
+                  </div>
+                    </div>
+                      <div class="col-xl-3 pb-4">
+                     
+                        <div class="card cardMainImage h-100">
+                                <div class="d-flex justify-content-between align-items-center mb-3 cardText">
+                                    <span >PAN Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                          <div class="d-flex justify-content-between align-items-center mb-2 cardText">
+                                    <span >PAN Number:</span>
+                      <p class="mb-0">HDH6545</p>
+                    </div>
+                             <img
+                                    src="<?= $this->params['baseurl'] ?>/images/prof.png" class="card-img-top object-fit-cover w-100"
+                                    alt="">
+                    
+                   
+                  </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="fivcont" role="tabpanel" aria-labelledby="contact-tab">
+                <div class="row">
+                    <div class="col-xl-9">
+                     <div class="overviewDataParent">
+                    <table class="table w-100 border-0 border_o d-inline-block py-3 bg-white">
+                        <tbody class="tbody-leads sighting-leads py-5 w-100">
+                            <tr>
+                                <td style="width: 60%;">Business Name:</td>
+                                <td style="width: 50%;">
+                                    <p>Shivsakti</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Address:</td>
+                                <td>
+                                    <p>Noida sector 62</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Phone Number: </td>
+                                <td>
+                                    <p>8825317553</p>
+                                </td>
+                            </tr>
+                                 <tr>
+                                <td>Legal entity Email:</td>
+                                <td>
+                                    <p>annu@gmail.com</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>Category:</td>
+                                <td>
+                                    <p>Safari Tour Operator</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>PAN Number:</td>
+                                <td>
+                                    <p>DFY1533SF</p>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>Approved Status:</td>
+                                <td>
+                                    <p>Yes</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                       </div>
+                    </div>
+                    <div class="col-xl-3">
+                        <div class="identification-photo">
+                            <p class="mb-1">Pan Photo: </p>
+                               <a href="/">
+                                  <img src="<?= $this->params['baseurl'] ?>/images/pancard.png" alt="" class="w-100 h-100 object-fit-cover">
+                              </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="tab-pane fade" id="svencont" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
             <div class="tab-pane fade" id="eighttcont" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
             <div class="tab-pane fade" id="ninetcont" role="tabpanel" aria-labelledby="contact-tab">comingsoon!</div>
