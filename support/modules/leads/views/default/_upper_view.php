d84ef702ef5687381d4e88b298baf0772cdf13a1
Abhinav <abhinavpal8960@gmail.com>
2025-07-25 12:58:43 +0530

    changes

--- support/modules/leads/views/default/_upper_view.php
+++ support/modules/leads/views/default/_upper_view.php
@@ -8,68 +8,20 @@ $this->params['baseurl'] = $webasset->baseUrl;
 <section class="listCard mx-3">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xxl-3 col-xl-4 col-md-6 col-12 mb-3">
+            <div class="col-xl-3">
                 <div class="mainCard py-3 px-3">
-                    <div class="cardChild d-flex justify-content-between align-items-center">
-                        <div class="d-flex gap-2 align-items-center">
-                            <div class="iconsDiv d-flex justify-content-center align-items-center">
-                                <img src="<?= $this->params['baseurl'] ?>/images/whislist.svg" alt="Wishlist">
-                            </div>
-                            <div class="text-card">
-                                <p>Current Active Leads</p>
-                            </div>
+                    <div class="cardChild">
+                        <div class="text-card mb-3">
+                            <p>Current Active Leads</p>
                         </div>
-                        <?php if ($leadModel) { ?>
-                            <div class="numbwrCount">
-                                <h3><?= $leadModel->activeLeadCount ?></h3>
+                        <div class="numbwrCount d-flex gap-5">
+                            <div class="iconsDiv mb-2 d-flex justify-content-center align-items-center">
+                                <img src="<?= $this->params['baseurl'] ?>/images/lead_dashboard.svg"
+                                    class="" alt="" style="width: 11px; height: 11px; object-fit: cover;">
                             </div>
-                        <?php } else {  ?>
-                            <div class="numbwrCount">
-                                <h3>0</h3>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xxl-3 col-xl-4 col-md-6 col-12 mb-3">
-                <div class="mainCard py-3 px-3">
-                    <div class="cardChild d-flex justify-content-between align-items-center">
-                        <div class="d-flex gap-2 align-items-center">
-                            <div class="iconsDiv d-flex justify-content-center align-items-center">
-                                <img src="<?= $this->params['baseurl'] ?>/images/quote_request.svg" alt="Quotes">
-                            </div>
-                            <div class="text-card">
-                                <p>Leads in Discussion</p>
-                            </div>
-                        </div>
-
-                        <?php if ($leadModel) { ?>
-                            <div class="numbwrCount">
-                                <h3><?= $leadModel->partner_chat_started_count ?></h3>
-                            </div>
-                        <?php } else {  ?>
-                            <div class="numbwrCount">
-                                <h3>0</h3>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-                <div class="col-xxl-3 col-xl-4 col-md-6 col-12 mb-3">
-                    <div class="mainCard py-3 px-3">
-                        <div class="cardChild d-flex justify-content-between align-items-center">
-                            <div class="d-flex gap-2 align-items-center">
-                                <div class="iconsDiv d-flex justify-content-center align-items-center">
-                                    <img src="<?= $this->params['baseurl'] ?>/images/Icon material-twotone-currency-rupee.svg" alt="Quotes">
-                                </div>
-                                <div class="text-card">
-                                    <p>Payment Received</p>
-                                </div>
-                            </div>
-
                             <?php if ($leadModel) { ?>
                                 <div class="numbwrCount">
-                                <h3><?= $leadModel->paymentReceivedCount ?></h3>
+                                    <h3><?= $leadModel->activeLeadCount ?></h3>
                                 </div>
                             <?php } else {  ?>
                                 <div class="numbwrCount">
@@ -79,34 +31,82 @@ $this->params['baseurl'] = $webasset->baseUrl;
                         </div>
                     </div>
                 </div>
-            <div class="col-xxl-3 col-xl-4 col-md-6 col-12 mb-3">
+            </div>
+            <div class="col-xl-3">
                 <div class="mainCard py-3 px-3">
-                    <div class="cardChild d-flex justify-content-between align-items-center">
-                        <div class="d-flex gap-2 align-items-center">
-                            <div class="iconsDiv d-flex justify-content-center align-items-center">
-                                <img src="<?= $this->params['baseurl'] ?>/images/package_comment.svg" alt="Comment">
-                            </div>
-                            <div class="text-card">
-                                <p>Pending Payments</p>
-                            </div>
+                    <div class="cardChild">
+                        <div class="text-card mb-3">
+                            <p>Leads in Discussion</p>
                         </div>
-
-                        <?php if ($leadModel) { ?>
-                            <div class="numbwrCount">
-                                <h3><?= $leadModel->pendingPaymentCount ?></h3>
+                        <div class="numbwrCount d-flex gap-5">
+                            <div class="iconsDiv mb-2 d-flex justify-content-center align-items-center" style="background-color: #FFF4EE;">
+                                <img src="<?= $this->params['baseurl'] ?>/images/fixeddepa.png"
+                                    class="" alt="" style="width: 11px; height: 11px; object-fit: cover;">
                             </div>
-                        <?php } else {  ?>
-                            <div class="numbwrCount">
-                                <h3>0</h3>
+                            <?php if ($leadModel) { ?>
+                                <div class="numbwrCount">
+                                    <h3><?= $leadModel->partner_chat_started_count ?></h3>
+                                </div>
+                            <?php } else {  ?>
+                                <div class="numbwrCount">
+                                    <h3>0</h3>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3">
+                <div class="mainCard py-3 px-3">
+                    <div class="cardChild">
+                        <div class="text-card mb-3">
+                            <p>Payment Received</p>
+                        </div>
+                        <div class="numbwrCount d-flex gap-5">
+                            <div class="iconsDiv mb-2 d-flex justify-content-center align-items-center" style="background-color: #DDFFE7;">
+                                <img src="<?= $this->params['baseurl'] ?>/images/package.png"
+                                    class="" alt="" style="width: 11px; height: 11px; object-fit: cover;">
                             </div>
-                        <?php } ?>
+                            <?php if ($leadModel) { ?>
+                                <div class="numbwrCount">
+                                    <h3><?= $leadModel->paymentReceivedCount ?></h3>
+                                </div>
+                            <?php } else {  ?>
+                                <div class="numbwrCount">
+                                    <h3>0</h3>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3">
+                <div class="mainCard py-3 px-3">
+                    <div class="cardChild">
+                        <div class="text-card mb-3">
+                            <p>Pending Payments</p>
+                        </div>
+                        <div class="numbwrCount d-flex gap-5">
+                            <div class="iconsDiv mb-2 d-flex justify-content-center align-items-center" style="background-color: #AAAAAA;">
+                                <img src="<?= $this->params['baseurl'] ?>/images/qut.png"
+                                    class="" alt="" style="width: 11px; height: 11px; object-fit: cover;">
+                            </div>
+                            <?php if ($leadModel) { ?>
+                                <div class="numbwrCount">
+                                    <h3><?= $leadModel->pendingPaymentCount ?></h3>
+                                </div>
+                            <?php } else {  ?>
+                                <div class="numbwrCount">
+                                    <h3>0</h3>
+                                </div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
 
 
 <style>
