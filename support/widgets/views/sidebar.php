30baddebacc9da8345bcdf481a9d75cc33d3e323
Abhinav <abhinavpal8960@gmail.com>
2025-07-25 14:43:50 +0530

    changes

--- support/widgets/views/sidebar.php
+++ support/widgets/views/sidebar.php
@@ -69,7 +69,6 @@ $this->params['baseurl'] = $webasset->baseUrl;
             </a>
         </li>
 
-<?php if(false){?>
         <li class="navItems mb-2 position-relative <?= in_array($active_url, array(
                                                         "/operator/safari-operator/index",
                                                         "/operator/safari-operator/view",
@@ -241,7 +240,6 @@ $this->params['baseurl'] = $webasset->baseUrl;
             </ul>
 
         </li>
-<?php } ?>
 
         <li class="navItems navitemLogout mb-2 ms-0">
             <a class="navLinks  d-flex align-items-center" href="<?= \yii\helpers\Url::to('/site/logout') ?>" data-method="post">
