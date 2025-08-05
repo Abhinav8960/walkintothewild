d05e52edface6b9269f28a4f62929fc996a0f062
Abhinav <abhinavpal8960@gmail.com>
2025-08-05 14:22:21 +0530

    changes

--- support/modules/sharesafari/views/default/_fixed_view.php
+++ support/modules/sharesafari/views/default/_fixed_view.php
@@ -29,31 +29,6 @@ $this->title = 'Fixed Departure';
                                     <div class="itrnTextCard py-4">
                                         <h6 class="pb-3">About Trip / Overview</h6>
                                         <p><?= $share_safari->safari_plan ?></p>
-                                        <?php if ($share_safari->partner_gallery_id && !empty($share_safari->gallery_json)) {
-                                            $gallery_images = json_decode($share_safari->gallery_json, true);
-                                            $images = $gallery_images['images'];
-                                        ?>
-                                            <h6>Accomodation Images</h6>
-                                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                                                <!-- <div class="carousel-indicators">
-                                                            <?php foreach ($images as $index => $image) { ?>
-                                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $index ?>" class="<?= $index === 0 ? 'active' : '' ?>" aria-current="<?= $index === 0 ? 'true' : 'false' ?>" aria-label="Slide <?= $index + 1 ?>"></button>
-                                                            <?php } ?>
-                                                        </div> -->
-                                                <div class="carousel-inner">
-                                                    <?php foreach ($images as $index => $image) { ?>
-                                                        <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
-                                                            <img class="d-block w-100 rounded carousel-img" src="<?= $image['gallery_image_path'] ?>" alt="<?= htmlspecialchars($image['title']) ?>">
-                                                            <div class="carousel-caption d-none d-md-block">
-                                                                <h5><?= $image['title'] ?></h5>
-                                                                <p><?= $image['caption'] ?></p>
-                                                            </div>
-                                                        </div>
-                                                    <?php } ?>
-                                                </div>
-                                            </div>
-                                        <?php } ?>
-
                                     </div>
                                 </div>
 
