b22ce6cbb31726f187f2c8909a812468cbf912b5
Akash <akashprajapatiak05@gmail.com>
2025-08-05 11:11:52 +0530

    Some change in view

--- business/modules/package/views/default/_upper_view.php
+++ business/modules/package/views/default/_upper_view.php
@@ -18,17 +18,19 @@ $this->params['baseurl'] = $webasset->baseUrl;
                         <h2>Package : <?= mb_strimwidth($package->package_name, 0, 40, "...") ?></h2>
                     </div>
                     <div class="butonsParent d-flex align-items-center gap-3">
-                        <?php if ($package->package->edit_status == 1) { ?>
-                            <div class="col-lg-2">
-                                <div class="editBtn float-end">
-                                    <?= Html::a('Edit', [Url::toRoute(['update', 'id' => $package->package_id])], ['title' => 'Edit']) ?>
+                        <?php if ($var = $package->package) { ?>
+                            <?php if ($var->edit_status == 1) { ?>
+                                <div class="col-lg-2">
+                                    <div class="editBtn float-end">
+                                        <?= Html::a('Edit', [Url::toRoute(['update', 'id' => $package->package_id])], ['title' => 'Edit']) ?>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($package->package->status != 1) { ?>
-                            <div class="edinBtn">
-                                <?= Html::a('Send For Approval', [Url::toRoute(['send-for-approval', 'id' => $package->id])], ['title' => 'Send For Approval']) ?>
-                            </div>
+                            <?php } ?>
+                            <?php if ($var->status != 1) { ?>
+                                <div class="edinBtn">
+                                    <?= Html::a('Send For Approval', [Url::toRoute(['send-for-approval', 'id' => $package->id])], ['title' => 'Send For Approval']) ?>
+                                </div>
+                            <?php } ?>
                         <?php } ?>
                     </div>
                 </div>
