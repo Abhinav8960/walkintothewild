242fe7be973e861d319b4ebc29d5c19006c2ac37
Anurag <mranurag101@gmail.com>
2025-08-08 18:44:02 +0530

    changes

---
+++ webhook/controllers/DeepCallController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace webhook\controllers;
+
+use api\models\chat\Chat;
+use api\models\chat\ChatMessage;
+use api\models\leads\LeadPartnerQuotes;
+use common\models\transaction\Transaction;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii;
+
+/**
+ * Default controller for the `error` module
+ */
+class DeepCall extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                    ],
+
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'index' => ['POST', 'GET']
+                ],
+            ],
+        ];
+    }
+
+
+    public function beforeAction($action)
+    {
+        if (in_array($action->id, ['index'])) {
+            // Disable CSRF validation for the payu-response action
+            $this->enableCsrfValidation = false;
+        }
+        return parent::beforeAction($action);
+    }
+
+    public function actionIndex()
+    {
+        $data = Yii::$app->request->post();
+        return "GODBLESSYOU";
+        \Yii::info('deep-call webhook: ' . date('Y-m-d H:i A') . '' . json_encode($data), 'deep-call');
+    }
+}
