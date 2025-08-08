834a9a1b069dd8a4114fd5ed783c37fc0fe50efa
Anurag <mranurag101@gmail.com>
2025-08-08 15:52:52 +0530

    changes

--- common/events/sharesafari/SafariBookedByUser.php
+++ common/events/sharesafari/SafariBookedByUser.php
@@ -19,36 +19,36 @@ class SafariBookedByUser extends Event
     public $channelName;
     public $shared_safari_id;
     public $shared_safari_name;
-    public $no_of_safari; 
+    public $no_of_safari;
     public $start_date;
-    public $end_date ;
-    public $total_seat;
+    public $end_date;
+    public $booked_seat;
     public $type;
     public $shared_safari_title;
-    public $receiverUserIds = [];
-    public $shared_safari_url;
-    protected $sent_data = [];
+    public $transactionId;
+    public $refencerId;
+    public $amount;
     protected $shared_safari;
     protected $engine;
-    protected $master_notification_template;
 
     protected $channels = [
         'email',
         // 'firebase',
     ];
 
-    protected $mail_template_code = 'NSCU';  // New Safari Created By User mail to admin
-    protected $mail_template_code_follower = 'NSTF';  // New Safari Created By User mail to followers
+    protected $mail_template_code = 'SSBU';  // New Safari Created By User mail to admin
 
 
-    public function __construct(array $receiverUserIds,$userId, $email, $name ,$shared_safari_id)
+    public function __construct($userId, $email, $name, $shared_safari_id, $transactionId, $refencerId, $amount)
     {
-        
-        $this->receiverUserIds = $receiverUserIds;
+
         $this->userId = $userId;
         $this->email = $email;
         $this->name = $name;
         $this->shared_safari_id = $shared_safari_id;
+        $this->transactionId = $transactionId;
+        $this->refencerId = $refencerId;
+        $this->amount = $amount;
         $this->engine = \Yii::$app->engine;
         $this->prepareData();
         $this->broadcast();
@@ -56,7 +56,7 @@ class SafariBookedByUser extends Event
 
     public function broadcast()
     {
-       
+
         foreach ($this->channels as $channel) {
             $this->channelName = $channel;
             $this->templates = $this->getTemplates()[$channel];
@@ -72,43 +72,23 @@ class SafariBookedByUser extends Event
         $arr = [
             'email' => [
                 [
-                    'subject' => 'Check Out new Safari !!',
+                    'subject' => $this->shared_safari_title . 'Booked, get ready for adventure!!',
                     'mail_template_id' => $this->emailTemplateId(),
                     'params' => [
                         'username' => $this->name,
                         'email' => $this->email,
                         // 'shared_safari' => $this->shared_safari_name,
-                        'shared_safari_title'=>$this->shared_safari_title,
-                        'no_of_safari'=>$this->no_of_safari,
-                        'start_date'=>$this->start_date,
-                        'end_date'=>$this->end_date,
-                        'total_seat'=>$this->total_seat,
-                        'type'=>$this->type,
-                        'shared_safari_url' => $this->shared_safari_url,
+                        'shared_safari_title' => $this->shared_safari_title,
+                        'no_of_safari' => $this->no_of_safari,
+                        'start_date' => $this->start_date,
+                        'end_date' => $this->end_date,
+                        'booked_seat' => $this->booked_seat,
                     ],
                     'to_mail' => Yii::$app->params['adminEmail'],
                     'cc' => [],
                     'bcc' => [],
-                ],
-                [
-                    'subject' => 'Check Out new Safari !!',
-                    'mail_template_id' => $this->emailTemplateIdFollwer(),
-                    'params' => [
-                        'username' => $this->name,
-                        'email' => $this->email,
-                        // 'shared_safari' => $this->shared_safari_name,
-                        'shared_safari_title'=>$this->shared_safari_title,
-                        'no_of_safari'=>$this->no_of_safari,
-                        'start_date'=>$this->start_date,
-                        'end_date'=>$this->end_date,
-                        'total_seat'=>$this->total_seat,
-                        'type'=>$this->type,
-                        'shared_safari_url' => $this->shared_safari_url,
-                    ],
-                    'to_mail' => $this->email,
-                    'cc' => [],
-                    'bcc' => [],
-                ],
+                ]
+
             ],
             'firebase' => $this->prepareFirebaseTemplate()
         ];
@@ -124,78 +104,26 @@ class SafariBookedByUser extends Event
         return null;
     }
 
-    protected function emailTemplateIdFollwer()
-    {
-        $template = MasterMailTemplate::find()->where(['code' => $this->mail_template_code_follower, 'status' => 1])->limit(1)->one();
-        if ($template) {
-            return $template->id;
-        }
-        return null;
-    }
 
-    protected function firebaseTemplateId()
-    {
-        $this->master_notification_template = MasterNotificationTemplate::find()->where(['id' => MasterNotificationTemplate::NEW_SAFARI_CREATED, 'status' => 1])->limit(1)->one();
-        if ($this->master_notification_template) {
-            return $this->master_notification_template->id;
-        }
-        return null;
-    }
 
-    private function title()
-    {
-        return $this->engine->render($this->master_notification_template->title, []);
-    }
-
-    private function message()
-    {
-        return $this->engine->render($this->master_notification_template->message, ['username'=>$this->name]);
-    }
 
     public function prepareData()
     {
-
         $this->shared_safari = ShareSafari::find()->where(['id' => $this->shared_safari_id])->one();
         $this->shared_safari_title = $this->shared_safari->share_safari_title;
         $this->no_of_safari = $this->shared_safari->no_of_safari;
         $this->start_date = $this->shared_safari->start_date;
-        $this->end_date = $this->shared_safari-> end_date;
-        $this->total_seat = $this->shared_safari->total_seat;
-        $this->type =$this->shared_safari->type;
-        $this->shared_safari_url = urlencode(\Yii::$app->frontendUrlManager->createAbsoluteUrl(['/sharedsafari/default/view', 'slug' => $this->shared_safari->slug, 'organized_slug' => $this->shared_safari->organizedslug ? $this->shared_safari->organizedslug : '']));
-        $this->userId = $this->shared_safari->host_user_id;
-        if ($this->shared_safari->type == ShareSafari::TYPE_FIXED_DEPARTURE) {
-            $this->email = $this->shared_safari->safarioperator->email;
-            $this->name =  $this->shared_safari->safarioperator->business_name;
-        } else {
-            $this->email = $this->shared_safari->user->email;
-            $this->name =  $this->shared_safari->user->name;
-        }
-        $this->sent_data = [
-            'shared_safari_title' => $this->shared_safari->share_safari_title,
-            'slug' => $this->shared_safari->slug,
-            'no_of_safari'=>$this->no_of_safari,
-            'start_date'=>$this->start_date,
-            'end_date'=>$this->end_date,
-            'total_seat'=>$this->total_seat,
-            'type'=>$this->type
-        ];
-    }
+        $this->end_date = $this->shared_safari->end_date;
+        $this->booked_seat = $this->shared_safari->booked_seat;
 
 
-    private function prepareFirebaseTemplate()
-    {
-        foreach ($this->receiverUserIds as $userId) {
-            $arr[] =  [
-                'master_notification_template_id'   => $this->firebaseTemplateId(),
-                'title'                             => $this->title(),
-                'message'                           => $this->message(),
-                'sent_data'                         => $this->sent_data,
-                'user_id'                           => $userId,
-                'image_url'                         => NULL,
-                'action'                            => NULL,
-            ];
-        }
-        return $arr;
+        // $this->sent_data = [
+        //     'shared_safari_title' => $this->shared_safari->share_safari_title,
+        //     'slug' => $this->shared_safari->slug,
+        //     'no_of_safari' => $this->no_of_safari,
+        //     'start_date' => $this->start_date,
+        //     'end_date' => $this->end_date,
+        //     'booked_seat' => $this->booked_seat,
+        // ];
     }
 }
