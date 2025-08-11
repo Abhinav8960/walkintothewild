b321440096a9ce54d9ca48cccb6e7f40ec4ea2cf
Anurag <mranurag101@gmail.com>
2025-08-11 14:52:17 +0530

    working on calling api

--- webhook/controllers/DeepCallController.php
+++ webhook/controllers/DeepCallController.php
@@ -28,7 +28,7 @@ class DeepCallController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['index','save-call-log'],
+                        'actions' => ['index', 'save-call-log'],
                         'allow' => true,
                     ],
 
@@ -59,19 +59,23 @@ class DeepCallController extends Controller
         $pushReport = Yii::$app->request->post('push_report');
         \Yii::info('deep-call webhook: ' . date('Y-m-d H:i A') . '' . json_encode($data), 'deep-call');
 
+        // First remove escaped quotes and decode
+        $jsonString = stripslashes($pushReport);
+
         // First decode the JSON string
-        $decodedReport = json_decode($pushReport, true);
-        
+        $data = json_decode($jsonString, true);
+
+
+
         if (json_last_error() !== JSON_ERROR_NONE) {
             Yii::error('JSON decode error: ' . json_last_error_msg(), 'deep-call');
             throw new \Exception('Invalid JSON format');
         }
 
-        
+
         if (Yii::$app->request->isPost) {
             // Parse the nested data
-           $data = json_decode($decodedReport['push_report'], true);
-            
+
             if (isset($data['api_para']['userId']) && isset($data['api_para']['reqId'])) {
                 $userId = $data['api_para']['userId'];
                 $reqId = $data['api_para']['reqId'];
@@ -87,14 +91,10 @@ class DeepCallController extends Controller
         return "GODBLESSYOU";
     }
 
-    // private function saveCallLog($data)
-    public function actionSaveCallLog()
+    // public function actionSaveCallLog()
+    private function saveCallLog($data)
     {
-       // First remove escaped quotes and decode
-        $jsonString = stripslashes('{\"api_para\":{\"userId\":\"99985561\",\"fromType\":\"number\",\"from\":\"9650901148\",\"toType\":\"number\",\"to\":\"9315723354\",\"reqId\":\"rIhwk_1754661511_W_jMI\"},\"contactListData\":{},\"did\":null,\"cType\":\"CTC\",\"campId\":0,\"excAdm\":75,\"ivrSTime\":\"2025-08-08 19:28:32\",\"ivrETime\":\"2025-08-08 19:28:51\",\"ivrDuration\":19,\"userId\":\"99985561\",\"cNumber\":\"919315723354\",\"masterNumCTC\":\"919650901148\",\"masterAgent\":\"\",\"masterAgentNumber\":\"\",\"masterGroupId\":0,\"talkDuration\":8,\"agentOnCallDuration\":0,\"callId\":\"2me2nb2r2175466151149712937\",\"firstAttended\":\"from\",\"firstAnswerTime\":\"2025-08-08 19:28:35\",\"lastHangupTime\":\"2025-08-08 19:28:51\",\"lastFirstDuration\":16,\"custAnswerSTime\":\"2025-08-08 19:28:43\",\"custAnswerETime\":\"2025-08-08 19:28:51\",\"custAnswerDuration\":48,\"callStatus\":3,\"ivrExecuteFlow\":null,\"HangupBySourceDetected\":0,\"forward\":\"false\",\"totalHoldDuration\":0,\"totalCreditsUsed\":{\"freeHit\":2,\"paidHit\":0,\"amount\":0},\"ivrIdArr\":[],\"aAnsH\":[],\"aH\":[],\"nH\":[\"919315723354\",\"919650901148\"],\"recordings\":[{\"nodeid\":\"to\",\"visitId\":\"1754661514686\",\"file\":\"/202508/2me2nb2r2175466151149712937_9315723354_2025-8-8-19-28-43_CTC.mp3\",\"time\":\"2025-08-08 19:28:43\"}],\"voiceMail\":[],\"DTMF\":[],\"cliArr\":{\"9650901148\":[\"917557180901\"],\"9315723354\":[\"917557180901\"]},\"aHDetail\":[],\"nHDetail\":[{\"CTC\":\"to\",\"status\":\"answered\",\"recording\":\"/202508/2me2nb2r2175466151149712937_9315723354_2025-8-8-19-28-43_CTC.mp3\",\"ping\":\"direct\",\"number\":\"919315723354\",\"visitId\":\"1754661514686\",\"nodeId\":\"#1\",\"totalRingDuration\":8,\"totalHoldDuration\":0,\"talkDuration\":8,\"talkSTime\":\"2025-08-08 19:28:43\",\"talkETime\":\"2025-08-08 19:28:51\",\"answerSTime\":\"2025-08-08 19:28:43\",\"answerETime\":\"2025-08-08 19:28:51\",\"answerDuration\":8,\"cli\":\"917557180901\",\"retry\":0,\"recordingUrl\":\"https://s-ct3.sarv.com/v2/recording/direct/99985561/202508/2me2nb2r2175466151149712937_9315723354_2025-8-8-19-28-43_CTC.mp3\"},{\"CTC\":\"from\",\"status\":\"answered\",\"recording\":\"/202508/2me2nb2r2175466151149712937_9315723354_2025-8-8-19-28-43_CTC.mp3\",\"ping\":\"direct\",\"number\":\"919650901148\",\"visitId\":\"1754661511518\",\"nodeId\":\"#1\",\"totalRingDuration\":3,\"totalHoldDuration\":0,\"talkDuration\":8,\"talkSTime\":\"2025-08-08 19:28:43\",\"talkETime\":\"2025-08-08 19:28:51\",\"answerSTime\":\"2025-08-08 19:28:35\",\"answerETime\":\"2025-08-08 19:28:51\",\"answerDuration\":16,\"cli\":\"917557180901\",\"retry\":0,\"recordingUrl\":\"https://s-ct3.sarv.com/v2/recording/direct/99985561/202508/2me2nb2r2175466151149712937_9315723354_2025-8-8-19-28-43_CTC.mp3\"}],\"modules\":[],\"callDisposition\":\"[]\",\"callBack\":\"\"}');
-        
-            // Decode JSON with error checking
-        $data = json_decode($jsonString, true);
+
 
         $callLog = \common\models\CallLog::find()->where(['reference_id' => $data['api_para']['reqId']])->one();
         $callLog->service = \common\models\CallLog::SERVICE_DEEP_CALL;
