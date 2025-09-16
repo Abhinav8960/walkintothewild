89a7a2bc87c1bf08e691c4d09bb5011352fdc3e5
Anurag <mranurag101@gmail.com>
2025-09-16 17:24:18 +0530

    changes

--- common/mail/payment_received_against_quotation_for_operator-html.php
+++ common/mail/payment_received_against_quotation_for_operator-html.php
@@ -78,7 +78,7 @@ use yii\helpers\Html;
         if (isset($travelersDetails)) {
         ?>
             <div style="background-color: #a1a1a1; padding: 20px; border-radius: 8px; margin-bottom: 30px;">
-                <div style="font-size: 18px; font-weight: bold; color: #333; text-align: center; margin-bottom: 15px;">Travelers Details</div>
+                <div style="font-size: 18px; font-weight: bold; color: #333; text-align: center; margin-bottom: 15px;">Traveler Detail</div>
                 <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
                     <tr>
                         <td style="padding: 8px 0; font-weight: bold; width: 40%; color: #555;">Name</td>
