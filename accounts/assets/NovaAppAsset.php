0b69490ba030c52c2385ffbc80ace106449cfc63
Akash <akashprajapatiak05@gmail.com>
2025-09-18 18:10:16 +0530

    Theme set

--- accounts/assets/NovaAppAsset.php
+++ accounts/assets/NovaAppAsset.php
@@ -7,31 +7,19 @@ use yii\web\AssetBundle;
 class NovaAppAsset extends AssetBundle
 {
 
-    public $sourcePath = '@backend/themes/nova/assets/';
+    public $sourcePath = '@accounts/themes/nova/assets/';
     public $css = [
         'css/icons.css',
         'css/skin-modes.css',
-        'css/style-dark.css',
-        'css/style-transparent.css',
         'css/style.css',
-        'css/chat.css',
-        'plugins/bootstrap/css/bootstrap.min.css',
-        'plugins/perfect-scrollbar/p-scrollbar.css',
-        'plugins/sidebar/sidebar.css',
-        'plugins/select2/css/select2.min.css',
-        'plugins/datatable/css/dataTables.bootstrap5.css',
-        'plugins/datatable/css/buttons.bootstrap5.min.css',
-        'plugins/datatable/responsive.bootstrap5.css',
-        'https://uicdn.toast.com/editor/latest/toastui-editor.min.css',
-        'https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.css',
-        'https://uicdn.toast.com/editor-plugin-color-syntax/latest/toastui-editor-plugin-color-syntax.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css',
+        
     ];
     public $publishOptions = [
         'forceCopy' => true,
     ];
     public $js = [
         'js/script.js',
+        'plugins/accordion/accordion.min.js',
         'plugins/bootstrap/js/popper.min.js',
         'plugins/chart.js/Chart.bundle.min.js',
         'plugins/ionicons/ionicons.js',
@@ -58,12 +46,14 @@ class NovaAppAsset extends AssetBundle
         'js/themecolor.js',
         'https://unpkg.com/bootstrap-table@1.19.1/dist/bootstrap-table.min.js',
         'https://unpkg.com/bootstrap-table@1.19.1/dist/extensions/sticky-header/bootstrap-table-sticky-header.min.js',
+
         'https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js',
         'https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.js',
         'https://uicdn.toast.com/editor-plugin-color-syntax/latest/toastui-editor-plugin-color-syntax.min.js',
         'https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight.min.js',
         'https://cdn.ckeditor.com/ckeditor5/35.3.2/super-build/ckeditor.js',
-        'https://code.highcharts.com/highcharts.js'
+        'https://code.highcharts.com/highcharts.js',
+        
     ];
 
     public $depends = [
