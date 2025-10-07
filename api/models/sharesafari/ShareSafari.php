3c9ebd48d4079db37d10362b8c0bf13109363d47
Akash <akashprajapatiak05@gmail.com>
2025-10-07 10:58:21 +0530

    Template Code

--- api/models/sharesafari/ShareSafari.php
+++ api/models/sharesafari/ShareSafari.php
@@ -99,6 +99,7 @@ class ShareSafari extends \common\models\sharesafari\ShareSafari
             'witw_average_rating',
             'witw_review_count',
             'is_safari_operator',
+            'template_code',
             'status',
         ]);
         return $fields;
