6ed940f551c249c05058342b9f0965329d009fe6
Akash <akashprajapatiak05@gmail.com>
2025-11-18 10:07:27 +0530

    refactor(code): remove unecessary code

--- backend/modules/operator/views/safari-operator/_navbar.php
+++ backend/modules/operator/views/safari-operator/_navbar.php
@@ -83,7 +83,7 @@ use yii\helpers\Url;
         <!-- Tabs -->
         <ul class="nav panel-tabs main-nav-line">
             <li><a href="/operator/safari-operator/view?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'overview' ? 'active' : '' ?>">Overview</a></li>
-            <li><a href="/operator/safari-operator/quote?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'quote' ? 'active' : '' ?>">Get a Free Quote</a></li>
+            <!-- <li><a href="/operator/safari-operator/quote?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'quote' ? 'active' : '' ?>">Get a Free Quote</a></li> -->
             <!-- <li><a href="/operator/safari-operator/sharedsafari?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'sharedsafari' ? 'active' : '' ?>">Shared Safari</a></li> -->
             <li><a href="/operator/safari-operator/review?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'review' ? 'active' : '' ?>">User Review</a></li>
             <!-- <li><a href="/operator/safari-operator/follower?id=<?= $model->id ?>" class="nav-link <?= $active_navbar == 'follower' ? 'active' : '' ?>">Follower</a></li> -->
