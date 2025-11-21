42e37448c9a2696a958cf3232f67ce36cd53eb16
Akash <akashprajapatiak05@gmail.com>
2025-11-21 16:48:06 +0530

    refactor(api): user following logic enhance

--- api/models/UserFollow.php
+++ api/models/UserFollow.php
@@ -42,7 +42,10 @@ class UserFollow extends \common\models\UserFollow
                 foreach ($this->follower->toArray() as $key => $value) {
                     $fields[$key] = function () use ($key) {
                         if ($key == 'is_safari_operator') {
-                            return (bool) $this->user->is_safari_operator;
+                            return (bool) $this->follower->is_safari_operator;
+                        }
+                        if ($key == 'is_blue_badge_verified') {
+                            return (bool) $this->follower->is_blue_badge_verified;
                         }
                         return $this->follower->{$key};
                     };
